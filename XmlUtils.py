c9b1fce5b870bb27e82ac526b17a728a3515c184
Eric Nedervold <enedervold@seti.org>
2016-02-22 14:57:02 -0800

    Broke out XmlUtils.

--- XmlUtils.py
+++ XmlUtils.py
@@ -1,33 +1,12 @@
 import abc
-import os
-import os.path
 import xml.dom
 
-import ArchiveComponent
-import Info
-import XmlUtils
 
+class XmlUtils(object):
+    __metaclass__ = abc.ABCMeta
 
-class LabelMaker(XmlUtils.XmlUtils):
-    def __init__(self, component, info):
-        assert isinstance(component, ArchiveComponent.ArchiveComponent)
-        self.component = component
-        assert isinstance(info, Info.Info)
-        self.info = info
-        document = xml.dom.getDOMImplementation().createDocument(None,
-                                                                 None,
-                                                                 None)
-        XmlUtils.XmlUtils.__init__(self, document)
-
-        self.createDefaultXml()
-
-    @abc.abstractmethod
-    def createDefaultXml(self):
-        pass
-
-    @abc.abstractmethod
-    def defaultXmlName(self):
-        pass
+    def __init__(self, document):
+        self.document = document
 
     def _addProcessingInstruction(self, lhs, rhs):
         d = self.document
@@ -64,19 +43,3 @@ class LabelMaker(XmlUtils.XmlUtils):
                                                            encoding='utf-8'))
             else:
                 outputFile.write(self.document.toxml(encoding='utf-8'))
-
-
-def xmlSchemaCheck(filepath):
-    cmdTemplate = 'xmllint --noout --schema %s %s'
-    exitCode = os.system(cmdTemplate %
-                         ('./xml/PDS4_PDS_1500.xsd.xml', filepath))
-    return exitCode == 0
-
-
-def schematronCheck(filepath):
-    cmdTemplate = 'java -jar probatron.jar %s %s' + \
-        ' | xmllint -format -' + \
-        ' | python Schematron.py'
-    exitCode = os.system(cmdTemplate %
-                         (filepath, './xml/PDS4_PDS_1500.sch.xml'))
-    return exitCode == 0
