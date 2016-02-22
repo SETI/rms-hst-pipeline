2ab0294a4500e489cfdd0454b597f96642f3ecd4
Eric Nedervold <enedervold@seti.org>
2016-02-22 14:26:11 -0800

    Now producing XML-schema- and Schematron-valid product labels (although the contents are nonsense).

--- ProductInfo.py
+++ ProductInfo.py
@@ -11,25 +11,38 @@ class ProductInfo(Info.Info):
         return self.PLACEHOLDER('title')
 
     def startDateTime(self):
+        # TODO This is wrong
+        return '2000-01-02Z'
         return self.PLACEHOLDER('startDateTime')
 
     def stopDateTime(self):
+        # TODO This is wrong
+        return '2000-01-02Z'
         return self.PLACEHOLDER('stopDateTime')
 
     def investigationAreaName(self):
         return self.PLACEHOLDER('investigationAreaName')
 
     def investigationAreaType(self):
+        # TODO This is wrong
+        return 'Mission'
         return self.PLACEHOLDER('investigationAreaType')
+
+    def internalReferenceType(self):
+        return 'data_to_investigation'
 
     def observingSystemComponentName(self):
         return self.PLACEHOLDER('observingSystemComponentName')
 
     def observingSystemComponentType(self):
+        # TODO This is wrong
+        return 'Instrument'
         return self.PLACEHOLDER('observingSystemComponentType')
 
     def targetIdentificationName(self):
         return self.PLACEHOLDER('targetIdentificationName')
 
     def targetIdentificationType(self):
+        # TODO This is wrong
+        return 'Planet'
         return self.PLACEHOLDER('targetIdentificationType')
--- ProductLabelMaker.py
+++ ProductLabelMaker.py
@@ -1,5 +1,9 @@
+import tempfile
+
+import ArchiveFile
 import FileArchives
 import LabelMaker
+import ProductFileInfo
 import ProductInfo
 
 
@@ -13,19 +17,24 @@ class ProductLabelMaker(LabelMaker.LabelMaker):
 
     def createDefaultXml(self):
         product = self.component
+
         root = self._createChild(self.document, 'Product_Observational')
-        id_area, obs_area, file_area = \
+
+        PDS = self.info.pdsNamespaceUrl()
+        root.setAttribute('xmlns', PDS)
+        root.setAttribute('xmlns:pds', PDS)
+
+        id_area, obs_area = \
             self._createChildren(root, ['Identification_Area',
-                                        'Observation_Area',
-                                        'File_Area_Observational'])
+                                        'Observation_Area'])
 
         log_id, vers_id, title, info_ver, prod_cls = \
             self._createChildren(id_area, [
-                'pds:logical_identifier',
-                'pds:version_id',
-                'pds:title',
-                'pds:information_model_version',
-                'pds:product_class'])
+                'logical_identifier',
+                'version_id',
+                'title',
+                'information_model_version',
+                'product_class'])
 
         self._setText(log_id, str(product.lid))
         self._setText(vers_id, self.info.versionID())
@@ -45,11 +54,14 @@ class ProductLabelMaker(LabelMaker.LabelMaker):
         self._setText(start_dt, self.info.startDateTime())
         self._setText(stop_dt, self.info.stopDateTime())
 
-        nm, ty, i_refs = \
+        nm, ty, i_ref = \
             self._createChildren(invest_area, ['name', 'type',
                                                'Internal_Reference'])
         self._setText(nm, self.info.investigationAreaName())
         self._setText(ty, self.info.investigationAreaType())
+
+        ref_ty, = self._createChildren(i_ref, ['reference_type'])
+        self._setText(ref_ty, self.info.internalReferenceType())
 
         obs_sys_comp = self._createChild(obs_sys, 'Observing_System_Component')
         nm, ty = self._createChildren(obs_sys_comp, ['name', 'type'])
@@ -61,14 +73,38 @@ class ProductLabelMaker(LabelMaker.LabelMaker):
         self._setText(ty, self.info.targetIdentificationType())
 
         for file in product.files():
-            self.createFileInfo(file_area, file)
+            self.createFileInfo(root, file)
 
-    def createFileInfo(self, file_area, f):
+    def createFileInfo(self, root, f):
+        assert isinstance(f, ArchiveFile.ArchiveFile)
+        file_area = self._createChild(root, 'File_Area_Observational')
+
+        fileInfo = ProductFileInfo.ProductFileInfo(file_area, f)
         file = self._createChild(file_area, 'File')
         file_name = self._createChild(file, 'file_name')
-        self._setText(file_name, f.basename)
-        self._createChild(file_area, 'Array')
+        self._setText(file_name, fileInfo.fileName())
 
+        # TODO These are the wrong contents; it's a placeholder.
+        arr = self._createChild(file_area, 'Array')
+        offset, axes, ax_ind_order, elmt_arr, axis_arr = \
+            self._createChildren(arr, ['offset', 'axes', 'axis_index_order',
+                                       'Element_Array', 'Axis_Array'])
+
+        offset.setAttribute('unit', 'byte')
+        self._setText(offset, '0')
+        self._setText(axes, '1')
+        self._setText(ax_ind_order, 'Last Index Fastest')  # TODO Abstract?
+
+        d_ty = self._createChild(elmt_arr, 'data_type')
+        self._setText(d_ty, 'UnsignedByte')
+
+        ax_nm, elmts, seq_num = \
+            self._createChildren(axis_arr, ['axis_name', 'elements',
+                                            'sequence_number'])
+
+        self._setText(ax_nm, 'kurt')  # TODO Wrong
+        self._setText(elmts, '1')  # TODO Wrong
+        self._setText(seq_num, '1')  # TODO Wrong
 
 def testSynthesis():
     a = FileArchives.getAnyArchive()
@@ -76,8 +112,11 @@ def testSynthesis():
         if b.proposalId() != 0:
             for c in b.collections():
                 for p in c.products():
+                    fp = '/tmp/foo.xml'
                     lm = ProductLabelMaker(p)
-                    lm.printDefaultXml()
-                    return
+                    lm.createDefaultXmlFile(fp)
+                    if not (LabelMaker.xmlSchemaCheck(fp) and
+                            LabelMaker.schematronCheck(fp)):
+                        return
 
 testSynthesis()
