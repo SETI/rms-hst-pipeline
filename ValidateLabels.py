0477eef634ba2f0a7d31b6addc31b9b6364faa5e
Eric Nedervold <enedervold@seti.org>
2016-05-12 17:45:44 -0700

    Added 'print's for instrumentation.

--- ValidateLabels.py
+++ ValidateLabels.py
@@ -74,9 +74,24 @@ class CanMakeValidProductLabelsReduction(ProductLabelReduction):
                                                       get_reduced_hdus_no_fail)
 
 
+class LogProductsReduction(Reduction):
+    def reduce_archive(self, archive_root, get_reduced_bundles):
+        get_reduced_bundles()
+
+    def reduce_bundle(self, archive, lid, get_reduced_collections):
+        get_reduced_collections()
+
+    def reduce_collection(self, archive, lid, get_reduced_products):
+        get_reduced_products()
+
+    def reduce_product(self, archive, lid, get_reduced_products):
+        print str(lid)
+
+
 class ValidateLabelsReduction(CompositeReduction):
     def __init__(self):
         CompositeReduction.__init__(self, [
+                LogProductsReduction(),
                 CanMakeValidBundleLabelsReduction(),
                 CanMakeValidCollectionLabelsReduction(),
                 CanMakeValidProductLabelsReduction()])
