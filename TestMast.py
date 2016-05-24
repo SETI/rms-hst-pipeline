d17d791a4a32785052cc6a22ae147e21a96ac39f
Eric Nedervold <enedervold@seti.org>
2016-05-24 16:22:39 -0700

    Collected keys for HST columns.

--- TestMast.py
+++ TestMast.py
@@ -39,9 +39,25 @@ def query_dicts(query_args):
 
 if __name__ == '__main__':
     query_args = {
-        'max_records': '1'
+        'max_records': '1',
+        'verb': '3'
         }
     ds = query_dicts(query_args)
     for d in ds:
         print d
+        print sorted(d.keys())
         break
+
+# Keys for all columns are: ['AEC', 'Apertures', 'Archive Class',
+# 'Archive Date', 'Asn ID', 'Bandwidth', 'Broad Category', 'COSTAR',
+# 'Central Wavelength', 'Dataset', 'Dec (J2000)', 'Dec V1 (J2000)',
+# 'Dispersion', 'Ecliptic Latitude', 'Ecliptic Longitude', 'Exp Flag',
+# 'Exp Time', 'FGS Lock', 'FOV Config', 'Filters/Gratings', 'Galactic
+# Latitude', 'Galactic Longitude', 'Generation Date', 'High-Level
+# Science Products', 'Instrument', 'Instrument Config', 'MT Flag',
+# 'Obs Type', 'Obset ID', 'Obsnum', 'Operating Mode', 'PA Aper', 'PI
+# Last Name', 'Pixel Res', 'Preview Name', 'Prog ID', 'Proposal ID',
+# 'RA (J2000)', 'Ra V1 (J2000)', 'Ref', 'Release Date', 'Spectral
+# Res', 'Spectrum End', 'Spectrum Start', 'Start Time', 'Status',
+# 'Stop Time', 'Sun Alt', 'Target Descrip', 'Target Name', 'V3 Pos
+# Angle']
