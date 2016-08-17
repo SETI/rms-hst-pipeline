135c946d8e8691d42e4862b31709a9e977e735ce
Eric Nedervold <enedervold@seti.org>
2016-08-17 16:15:46 -0700

    Added SQL calls to DatabaseArchive.

--- pdart/db/DatabaseArchive.py
+++ pdart/db/DatabaseArchive.py
@@ -1,8 +1,10 @@
+import os.path
 import sqlite3
 
 
 class DatabaseArchive:
     def __init__(self, database_filepath):
+        self.archive_dir = os.path.dirname(database_filepath)
         self.database_filepath = database_filepath
         self.connection = sqlite3.connect(database_filepath)
 
@@ -13,3 +15,14 @@ class DatabaseArchive:
         if self.is_open():
             self.connection.close()
             self.connection = None
+
+    def execute_sql(self, sql, arg_tuple):
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql, arg_tuple)
+            for res_tuple in cursor.fetchall():
+                yield res_tuple
+
+    def execute_sql_once(self, sql, arg_tuple):
+        with closing(self.connection.cursor()) as cursor:
+            cursor.execute(sql, arg_tuple)
+            return cursor.fetchone()
--- pdart/db/test_DatabaseArchive.py
+++ pdart/db/test_DatabaseArchive.py
@@ -10,11 +10,16 @@ from pdart.pds4.Archives import *
 
 class TestDatabaseArchive(unittest.TestCase):
     def test_DatabaseArchive(self):
-        # Calling DatabaseArchive() will either open a file or create
-        # a new one.  For that reason, I should probably use a tmpfile
-        # instead.
+        # Fails with no file
+        with self.assertRaises(Exception):
+            DatabaseArchive(None)
+
+        # Succeeds with filepath.
         filepath = tempfile.mktemp()
         da = DatabaseArchive(filepath)
         self.assertTrue(da.is_open())
         da.close()
         self.assertFalse(da.is_open())
+
+        # Verify archive_dir
+        self.assertEquals(os.path.dirname(filepath), da.archive_dir)
