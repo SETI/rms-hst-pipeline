73d04c78391251b43ab4643dac7389a02e3989fb
Eric Nedervold <enedervold@seti.org>
2016-12-22 12:00:00 -0800

    The Dev.py script now (optionally creates and) uses the bundle databases instead of the single archive database.

--- Dev.py
+++ Dev.py
@@ -1,14 +1,15 @@
 """
-**SCRIPT:** Build the database then build bundle, collection, and
-product labels, and collection inventories.  This is a temporary
-script under active development and so is underdocumented.
+**SCRIPT:** Build the bundle databases then build bundle, collection,
+and product labels, and collection inventories.  Uses the bundle
+databases.
 """
 from contextlib import closing
 import os.path
 import sqlite3
 
-from pdart.db.CreateDatabase import ArchiveDatabaseCreator
+from pdart.db.CreateBundleDatabase import BundleDatabaseCreator
 from pdart.db.DatabaseName import DATABASE_NAME
+from pdart.pds4.Archive import Archive
 from pdart.pds4.Archives import *
 from pdart.pds4labels.BundleLabel import *
 from pdart.pds4labels.CollectionLabel import *
@@ -27,17 +28,19 @@ CREATE_DB = True
 # type: bool
 
 
-def make_db_labels(conn):
-    # type: (sqlite3.Connection) -> None
+def make_db_labels(archive):
+    # type: (Archive) -> None
     """
     Write labels for the whole archive in hierarchical order to
     increase cache hits for bundles and collections.  NOTE: This
     doesn't seem to run any faster, perhaps because of extra cost to
     having three open cursors.
     """
-    with closing(conn.cursor()) as bundle_cursor:
-        for (bundle,) in get_all_bundles(bundle_cursor):
-
+    for bundle_obj in archive.bundles():
+        bundle = bundle_obj.lid.lid
+        database_fp = os.path.join(bundle_obj.absolute_filepath(),
+                                   DATABASE_NAME)
+        with closing(sqlite3.connect(database_fp)) as conn:
             with closing(conn.cursor()) as collection_cursor:
                 for (coll,) in get_bundle_collections_db(collection_cursor,
                                                          bundle):
@@ -54,55 +57,14 @@ def make_db_labels(conn):
             make_db_bundle_label(conn, bundle, VERIFY)
 
 
-def make_db_bundle_labels(conn):
-    # type: (sqlite3.Connection) -> None
-    with closing(conn.cursor()) as cursor:
-        for (lid,) in cast(Iterable[Tuple[unicode]],
-                           cursor.execute('SELECT bundle FROM bundles')):
-            make_db_bundle_label(conn, lid, VERIFY)
-
-
-def make_db_collection_labels_and_inventories(conn):
-    # type: (sqlite3.Connection) -> None
-    with closing(conn.cursor()) as cursor:
-        for (lid,) in cast(Iterable[Tuple[unicode]],
-                           cursor.execute(
-                'SELECT collection FROM collections')):
-            make_db_collection_label_and_inventory(conn, lid, VERIFY)
-
-
-def make_db_product_labels(conn):
-    # type: (sqlite3.Connection) -> None
-    with closing(conn.cursor()) as cursor:
-        for (lid,) in cast(Iterable[Tuple[unicode]], cursor.execute(
-            """SELECT product FROM products EXCEPT
-               SELECT product FROM bad_fits_files""")):
-            make_db_product_label(conn, lid, VERIFY)
-
-
-def get_conn():
-    # type: () -> sqlite3.Connection
-    if IN_MEMORY:
-        return sqlite3.connect(':memory:')
-    else:
-        return sqlite3.connect(os.path.join(get_any_archive_dir(),
-                                            DATABASE_NAME))
-
-
 def dev():
     # type: () -> None
     archive = get_any_archive()
-    with closing(get_conn()) as conn:
-        if CREATE_DB:
-            ArchiveDatabaseCreator(conn, archive).create()
-        # It seems to run about the same, building labels
-        # hierarchically or by type.
-        if True:
-            make_db_labels(conn)
-        else:
-            make_db_product_labels(conn)
-            make_db_collection_labels_and_inventories(conn)
-            make_db_bundle_labels(conn)
+
+    if CREATE_DB:
+        BundleDatabaseCreator(archive).create()
+
+    make_db_labels(archive)
 
 
 if __name__ == '__main__':
