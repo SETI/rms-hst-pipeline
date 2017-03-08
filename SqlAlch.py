8209c7850db408eaac83228612faf7fe72f531df
Eric Nedervold <enedervold@seti.org>
2017-03-08 09:36:10 -0800

    Refactoring.

--- SqlAlch.py
+++ SqlAlch.py
@@ -12,9 +12,13 @@ from pdart.xml.Schema import verify_label_or_raise_fp
 import SqlAlchLabels
 from SqlAlchTables import *
 
-from typing import TYPE_CHECKING
+from typing import cast, TYPE_CHECKING
 if TYPE_CHECKING:
     import sqlite3
+    import pdart.pds4.Archive as A
+    import pdart.pds4.Bundle as B
+    import pdart.pds4.Collection as C
+    import pdart.pds4.Product as P
 
 
 def handle_undefined(val):
@@ -33,12 +37,12 @@ VERIFY = False
 
 
 def bundle_database_filepath(bundle):
-    # type: (Bundle) -> unicode
+    # type: (B.Bundle) -> unicode
     return os.path.join(bundle.absolute_filepath(), _NEW_DATABASE_NAME)
 
 
 def open_bundle_database(bundle):
-    # type: (Bundle) -> sqlite3.Connection
+    # type: (B.Bundle) -> sqlite3.Connection
     return sqlite3.connect(bundle_database_filepath(bundle))
 
 
@@ -52,88 +56,103 @@ def add_cards(session, product_lid, hdu_index, header):
     session.bulk_save_objects(cards)
 
 
+def add_bundle(archive, bundle):
+    # type: (A.Archive, B.Bundle) -> None
+    db_fp = bundle_database_filepath(bundle)
+    try:
+        os.remove(db_fp)
+    except OSError:
+        pass
+    engine = create_engine('sqlite:///' + db_fp)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    db_bundle = Bundle(lid=str(bundle.lid),
+                       proposal_id=bundle.proposal_id(),
+                       archive_path=os.path.abspath(archive.root),
+                       full_filepath=bundle.absolute_filepath(),
+                       label_filepath=bundle.label_filepath())
+    session.add(db_bundle)
+    session.commit()
+    for collection in bundle.collections():
+        add_collection(session, archive, bundle, collection)
+    print db_fp
+
+
+def add_collection(session, archive, bundle, collection):
+    # type: (Session, A.Archive, B.Bundle, C.Collection) -> None
+    db_collection = Collection(
+        lid=str(collection.lid),
+        bundle_lid=str(bundle.lid),
+        prefix=collection.prefix(),
+        suffix=collection.suffix(),
+        instrument=collection.instrument(),
+        full_filepath=collection.absolute_filepath(),
+        label_filepath=collection.label_filepath(),
+        inventory_name=collection.inventory_name(),
+        inventory_filepath=collection.inventory_filepath())
+    session.add(db_collection)
+    session.commit()
+    if collection.prefix() == 'data':
+        for product in collection.products():
+            add_product(session, archive, collection, product)
+
+
+def add_product(session, archive, collection, product):
+    # type: (Session, A.Archive, C.Collection, P.Product) -> None
+    db_fits_product = None
+    print '    ', product.lid
+    file = list(product.files())[0]
+    try:
+        with closing(pyfits.open(
+                file.full_filepath())) as fits:
+            db_fits_product = FitsProduct(
+                lid=str(product.lid),
+                collection_lid=str(collection.lid),
+                fits_filepath=file.full_filepath(),
+                label_filepath=product.label_filepath(),
+                visit=product.visit())
+            for (n, hdu) in enumerate(fits):
+                fileinfo = hdu.fileinfo()
+                db_hdu = Hdu(product_lid=str(product.lid),
+                             hdu_index=n,
+                             hdr_loc=fileinfo['hdrLoc'],
+                             dat_loc=fileinfo['datLoc'],
+                             dat_span=fileinfo['datSpan'])
+                session.add(db_hdu)
+                add_cards(session,
+                          str(product.lid),
+                          n,
+                          hdu.header)
+            session.add(db_fits_product)
+    except IOError as e:
+        db_bad_fits_file = BadFitsFile(
+            lid=str(product.lid),
+            filepath=file.full_filepath(),
+            message=str(e))
+        session.add(db_bad_fits_file)
+    session.commit()
+
+    if db_fits_product:
+        label = SqlAlchLabels.make_product_observational_label(
+            db_fits_product)
+        label_filepath = cast(str, db_fits_product.label_filepath)
+        with open(label_filepath, 'w') as f:
+            f.write(label)
+        try:
+            if VERIFY:
+                verify_label_or_raise_fp(label_filepath)
+            print '    ', 'label:', \
+                os.path.relpath(label_filepath, archive.root)
+        except:
+            print '#### failed on', label_filepath
+            raise
+
+
 def run():
     archive = get_any_archive()
     for bundle in archive.bundles():
-        db_fp = bundle_database_filepath(bundle)
-        try:
-            os.remove(db_fp)
-        except OSError:
-            pass
-        engine = create_engine('sqlite:///' + db_fp)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        db_bundle = Bundle(lid=str(bundle.lid),
-                           proposal_id=bundle.proposal_id(),
-                           archive_path=os.path.abspath(archive.root),
-                           full_filepath=bundle.absolute_filepath(),
-                           label_filepath=bundle.label_filepath())
-        session.add(db_bundle)
-        session.commit()
-        for collection in bundle.collections():
-            db_collection = Collection(
-                lid=str(collection.lid),
-                bundle_lid=str(bundle.lid),
-                prefix=collection.prefix(),
-                suffix=collection.suffix(),
-                instrument=collection.instrument(),
-                full_filepath=collection.absolute_filepath(),
-                label_filepath=collection.label_filepath(),
-                inventory_name=collection.inventory_name(),
-                inventory_filepath=collection.inventory_filepath())
-            session.add(db_collection)
-            session.commit()
-            if collection.prefix() == 'data':
-                for product in collection.products():
-                    db_fits_product = None
-                    print '    ', product.lid
-                    file = list(product.files())[0]
-                    try:
-                        with closing(pyfits.open(
-                                file.full_filepath())) as fits:
-                            db_fits_product = FitsProduct(
-                                lid=str(product.lid),
-                                collection_lid=str(collection.lid),
-                                fits_filepath=file.full_filepath(),
-                                label_filepath=product.label_filepath(),
-                                visit=product.visit())
-                            for (n, hdu) in enumerate(fits):
-                                fileinfo = hdu.fileinfo()
-                                db_hdu = Hdu(product_lid=str(product.lid),
-                                             hdu_index=n,
-                                             hdr_loc=fileinfo['hdrLoc'],
-                                             dat_loc=fileinfo['datLoc'],
-                                             dat_span=fileinfo['datSpan'])
-                                session.add(db_hdu)
-                                add_cards(session,
-                                          str(product.lid),
-                                          n,
-                                          hdu.header)
-                            session.add(db_fits_product)
-                    except IOError as e:
-                        db_bad_fits_file = BadFitsFile(
-                            lid=str(product.lid),
-                            filepath=file.full_filepath(),
-                            message=str(e))
-                        session.add(db_bad_fits_file)
-                    session.commit()
-
-                    if db_fits_product:
-                        label = SqlAlchLabels.make_product_observational_label(
-                            db_fits_product)
-                        label_filepath = db_fits_product.label_filepath
-                        with open(label_filepath, 'w') as f:
-                            f.write(label)
-                        try:
-                            if VERIFY:
-                                verify_label_or_raise_fp(label_filepath)
-                            print '    ', 'label:', \
-                                os.path.relpath(label_filepath, archive.root)
-                        except:
-                            print '#### failed on', label_filepath
-                            raise
-        print db_fp
+        add_bundle(archive, bundle)
 
 if __name__ == '__main__':
     run()
