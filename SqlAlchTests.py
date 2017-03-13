0fc77ad4cd12c4ab81eb9a23fdad457ba743c7cb
Eric Nedervold <enedervold@seti.org>
2017-03-13 11:02:30 -0700

    Started testing framework for the SqlAlchemy-based code.

---
+++ SqlAlchTests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestSqlAlch(unittest.TestCase):
+    def test(self):
+        # type: () -> None
+        self.assertEqual(2, 1+1)
+
+
+if __name__ == '__main__':
+    unittest.main()
