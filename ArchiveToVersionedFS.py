cd91faaf4a7f5d7410c38d5f638178f901a4399d
Eric Nedervold <enedervold@seti.org>
2017-08-16 10:25:00 -0700

    Have a working View on our standard directory structure as a versioned directory structure.

--- ArchiveToVersionedFS.py
+++ ArchiveToVersionedFS.py
@@ -10,7 +10,7 @@ from fs.errors import InvalidPath, ResourceNotFound
 from fs.error_tools import unwrap_errors
 from fs.info import Info
 from fs.osfs import OSFS
-from fs.path import basename, iteratepath, join
+from fs.path import abspath, basename, iteratepath, join, normpath
 
 from pdart.fs.ReadOnlyView import ReadOnlyView
 
@@ -18,8 +18,23 @@ if TYPE_CHECKING:
     from typing import Tuple
 
 
+_ROOT = u'/'
+
+
 def _is_root(path):
-    return abspath(normpath(path)) == u'/'
+    return abspath(normpath(path)) == _ROOT
+
+
+def _is_version_part(part):
+    return part.startswith(u'v$')
+
+_VERSION_ONE = u'v$1'
+
+_VISIT_DIR_PAT = u'visit_*'
+
+ALL_PAT = u'*'
+
+FILE_EXCLUSION_PATS = [u'.DS_Store', '*.db']
 
 
 class ArchiveToVersionedFS(ReadOnlyView):
@@ -40,21 +55,104 @@ class ArchiveToVersionedFS(ReadOnlyView):
                 if parts[1:]:
                     res = self._wrap_fs, join(*parts[1:])
                 else:
-                    res = self._wrap_fs, u'/'
+                    res = self._wrap_fs, _ROOT
             else:
                 raise ResourceNotFound(path)
         return res
 
+    def _get_visit_info(self, collection_path):
+        return self._wrap_fs.filterdir(
+            collection_path,
+            files=None, dirs=[_VISIT_DIR_PAT],
+            exclude_dirs=None, exclude_files=[ALL_PAT])
+
+    def _get_collection_fits_products(self, collection_path):
+        for visit_info in self._get_visit_info(collection_path):
+            for file in self._wrap_fs.listdir(join(collection_path,
+                                                   visit_info.name)):
+                if file.endswith(u'.fits'):
+                    yield file[:-5]
+
+    def _get_collection_product_files(self, collection_path, product):
+        for visit_info in self._get_visit_info(collection_path):
+            for file in self._wrap_fs.listdir(join(collection_path,
+                                                   visit_info.name)):
+                if file.startswith(product):
+                    yield file
+
+    def _get_product_filepath(self, collection_path, product_file):
+        for visit_info in self._get_visit_info(collection_path):
+            for file in self._wrap_fs.listdir(join(collection_path,
+                                                   visit_info.name)):
+                if file == product_file:
+                    return join(collection_path, visit_info.name, product_file)
+
+    def _get_files(self, path):
+        return [info.name for info in self._wrap_fs.filterdir(
+                path,
+                files=None, dirs=None,
+                exclude_dirs=[ALL_PAT], exclude_files=FILE_EXCLUSION_PATS)]
+
     def getinfo(self, path, namespaces=None):
         self.check()
-        _fs, _path = self._delegate_file_path(path)
-        if _fs:
-            with unwrap_errors(path):
-                raw_info = _fs.getinfo(_path, namespaces=namespaces).raw
-                if _is_root(_path):
-                    raw_info[u'basic'][u'name'] = self._bundle
+
+        def make_raw_info(name):
+            return {u'basic': {u'name': name, u'is_dir': True}}
+
+        parts = iteratepath(path)
+        l = len(parts)
+        if l == 0:
+            # path = /; purely synthetic
+            raw_info = make_raw_info(u'')
         else:
-            raw_info = {u'basic': {u'name': u'', u'is_dir': True}}
+            if not (parts[0] == self._bundle):
+                raise ResourceNotFound(path)
+            if l == 1:
+                # path = /b; orig path = /
+                raw_info = make_raw_info(self._bundle)
+            elif l == 2:
+                _b, c = parts
+                if _is_version_part(c):
+                    # path = b/$n
+                    raw_info = make_raw_info(c)
+                else:
+                    # path = b/c; orig path = c
+                    with unwrap_errors(path):
+                        raw_info = self._wrap_fs.getinfo(c).raw
+            elif l == 3:
+                _b, c, p = parts
+                if _is_version_part(c):
+                    # path = b/$n/file; orig path = /file
+                    with unwrap_errors(path):
+                        raw_info = self._wrap_fs.getinfo(p).raw
+                elif _is_version_part(p):
+                    # path = b/c/$n; orig path = /c
+                    raw_info = make_raw_info(p)
+                else:
+                    # path = b/c/p; synthetic
+                    raw_info = make_raw_info(p)
+            elif l == 4:
+                _b, c, p, f = parts
+                if _is_version_part(p):
+                    # path = b/c/$n/file; orig path /c/file
+                    with unwrap_errors(path):
+                        raw_info = self._wrap_fs.getinfo(join(c, f)).raw
+                elif _is_version_part(f):
+                    # path = b/c/p/$n; synthetic
+                    raw_info = make_raw_info(f)
+                else:
+                    assert False, 'getinfo(%s) has %d parts' % (path, l)
+            elif l == 5:
+                _b, c, p, v, f = parts
+                # path = b/c/p/$n/f; orig path /c/visit_?/f
+                assert _is_version_part(v), \
+                    'getinfo(%s) has %d parts' % (path, l)
+                with unwrap_errors(path):
+                    filepath = self._get_product_filepath(c, f)
+                    with unwrap_errors(path):
+                        raw_info = self._wrap_fs.getinfo(filepath).raw
+            else:
+                assert False, 'getinfo(%s) has %d parts' % (path, l)
         return Info(raw_info)
 
     def openbin(self, path, mode="r", buffering=-1, **options):
@@ -70,25 +168,72 @@ class ArchiveToVersionedFS(ReadOnlyView):
 
     def listdir(self, path):
         self.check()
-        _fs, _path = self._delegate_file_path(path)
-        with unwrap_errors(path):
-            if _fs:
-                dir_list = _fs.listdir(_path)
+        parts = iteratepath(path)
+        l = len(parts)
+        if l == 0:
+            dir_list = [self._bundle]
+        else:
+            if not (parts[0] == self._bundle):
+                raise ResourceNotFound(path)
+            if l == 1:
+                # path = b
+                dir_list = [info.name
+                            for info in self._wrap_fs.scandir(_ROOT)
+                            if info.is_dir]
+                dir_list.append(_VERSION_ONE)
+            elif l == 2:
+                b, c = parts
+                if _is_version_part(c):
+                    # path = b/$n
+                    dir_list = self._get_files(_ROOT)
+                else:
+                    # path = b/c
+                    dir_list = list(self._get_collection_fits_products(c))
+                    dir_list.append(_VERSION_ONE)
+            elif l == 3:
+                b, c, p = parts
+                if _is_version_part(c):
+                    # path = b/$n/p; orig path = /p
+                    if self._wrap_fs.exists(p):
+                        raise DirectoryExpected(path)
+                    else:
+                        raise ResourceNotFound(path)
+                elif _is_version_part(p):
+                    # path = b/c/$n; orig path = /c
+                    dir_list = self._get_files(c)
+                else:
+                    # path = b/c/p
+                    dir_list = [_VERSION_ONE]
+            elif l == 4:
+                b, c, p, v = parts
+                if _is_version_part(p):
+                    # path = b/c/$n/f; orig path /c/f
+                    if self._wrap_fs.exists(join(c, f)):
+                        raise DirectoryExpected(path)
+                    else:
+                        raise ResourceNotFound(path)
+                elif _is_version_part(v):
+                    # path= b/c/p/$i; orig path /c/v*
+                    dir_list = list(self._get_collection_product_files(c, p))
+                else:
+                    assert False, 'listdir(%s) has %d parts' % (path, l)
+            elif l == 5:
+                assert False, 'listdir(%s) has %d parts' % (path, l)
             else:
-                # handle root
-                dir_list = [self._bundle]
+                assert False, 'listdir(%s) has %d parts' % (path, l)
         return dir_list
 
 
 def test_fs():
-    return ArchiveToVersionedFS(u'/Users/spaceman/Desktop/Archive/hst_09678')
+    return ArchiveToVersionedFS(u'/Users/spaceman/Desktop/Archive/hst_14334')
 
 
 if __name__ == '__main__':
-    fs = ArchiveToVersionedFS(u'/Users/spaceman/Desktop/Archive/hst_09678')
+    fs = ArchiveToVersionedFS(u'/Users/spaceman/Desktop/Archive/hst_14334')
     # import pudb; pu.db
-    print "fs.listdir(u'/') = %s" % list(fs.listdir(u'/'))
-    print "fs.scandir(u'/') = %s" % list(fs.scandir(u'/'))
-    print "fs.filterdir(u'/') = %s" % list(fs.filterdir(u'/'))
+    print "fs.listdir(u'/hst_14334') = %s" % list(fs.listdir(u'/hst_14334'))
+    print "fs.scandir(u'/hst_14334') = %s" % list(fs.scandir(u'/hst_14334'))
+    print "fs.filterdir(u'/hst_14334') = %s" % \
+        list(fs.filterdir(u'/hst_14334'))
     fs.tree()
     print fs
