cf7d4ce47d9527bec923568ca1388479c9f890f6
Eric Nedervold <enedervold@seti.org>
2017-08-23 16:00:54 -0700

    Partial implementation of paths within the ArchiveToVersionedFS.

--- ArchiveToVersionedFS.py
+++ ArchiveToVersionedFS.py
@@ -1,6 +1,7 @@
 """
 Work on viewing an archive folder as a versioned filesystem.
 """
+from abc import *
 import io
 import pickle
 import sys
@@ -8,40 +9,317 @@ import traceback
 from typing import TYPE_CHECKING
 
 from fs.base import FS
-from fs.errors import InvalidPath, ResourceNotFound
+from fs.copy import copy_fs
+from fs.errors import FileExpected, InvalidPath, ResourceNotFound, \
+    ResourceReadOnly
 from fs.error_tools import unwrap_errors
 from fs.info import Info
 from fs.mode import check_writable
 from fs.osfs import OSFS
 from fs.path import abspath, basename, iteratepath, join, normpath
+from fs.tempfs import TempFS
 
 from pdart.fs.ReadOnlyView import ReadOnlyView
 
 if TYPE_CHECKING:
-    from typing import Tuple
+    from typing import Any, AnyStr, Tuple
 
 
 _ROOT = u'/'
+# type:unicode
+
+
+def _make_raw_dir_info(name):
+    # type: (unicode) -> Dict
+    return {u'basic': {u'name': name, u'is_dir': True}}
 
 
 def _is_root(path):
+    # type: (unicode) -> unicode
     return abspath(normpath(path)) == _ROOT
 
 
 def _is_version_part(part):
+    # type: (unicode) -> bool
     return part.startswith(u'v$')
 
 _VERSION_ONE = u'v$1'
+# type: unicode
 
 _VISIT_DIR_PAT = u'visit_*'
+# type: unicode
 
 _VERSION_DICT = u'version$dict'
+# type: unicode
 
 ALL_PAT = u'*'
+# type: unicode
 
 FILE_EXCLUSION_PATS = [u'.DS_Store', '*.db']
+# type: List[unicode]
 
 _RAW_VERSION_DICT_INFO = {u'basic': {u'name': _VERSION_DICT, u'is_dir': False}}
+# type: Dict
+
+
+class FSPath(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, fs, path):
+        # type: (OSFS, unicode) -> None
+        self._wrap_fs = fs
+        self._original_path = path
+
+    def __str__(self):
+        return '%s(%r, %r)' % (self.__class__.__name__,
+                               self._wrap_fs,
+                               self._original_path)
+
+    # @abstractmethod
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        assert False, '%s.getinfo(%r) unimplemented' % \
+            (self.__class__.__name__, self._original_path)
+
+    # @abstractmethod
+    def listdir(self):
+        # type: () -> List[unicode]
+        assert False, '%s.listdir(%r) unimplemented' % \
+            (self.__class__.__name__, self._original_path)
+
+    # @abstractmethod
+    def openbin(self, mode, buffering, **options):
+        # type: (AnyStr, int, **Any) -> Any
+        assert False, '%s.openbin(%r) unimplemented' % \
+            (self.__class__.__name__, self._original_path)
+
+
+# The decision-making for paths is complex and trying to analyse the
+# path and handle the different versions in getinfo(), listdir(), and
+# openbin() just gets too complex and error-prone.  We use a standard
+# object-oriented technique and make objects for the various path
+# types, and have each implement their own version of these methods.
+
+# Some of the paths are synthetic: that is, they don't match up with
+# paths in the underlying filesystem, while some are just renames of
+# existing paths.
+
+# We have ten kinds of paths:
+#     * the root path '/'
+#     * the bundle path '/hst_14334'
+#     * the collection path '/hst_14334/data_wfc3_raw'
+#     * the product path '/hst_14334/data_wfc3_raw/icwy01hdq_raw'
+#     * bundle version paths '/hst_14334/v$1'
+#     * collection version paths '/hst_14334/data_wfc3_raw/v$1'
+#     * product version paths '/hst_14334/data_wfc3_raw/icwy01hdq_raw/v$1'
+#     * bundle version filepaths '/hst_14334/v$1/bundle.xml'
+#     * collection version filepaths
+#           '/hst_14334/data_wfc3_raw/v$1/collection.xml'
+#     * product version filepaths
+#           '/hst_14334/data_wfc3_raw/icwy01hdq_raw/v$1/icwy01hdq_raw.fits'
+
+
+class FSRootPath(FSPath):
+    """
+    The root path '/'
+    """
+    def __init__(self, fs, path, bundle):
+        # type: (OSFS, unicode, unicode) -> None
+        assert path == _ROOT
+        FSPath.__init__(self, fs, _ROOT)
+        self._bundle = bundle
+
+    def openbin(self, mode, buffering, **options):
+        # type: (AnyStr, int, **Any) -> Any
+        raise FileExpected(_ROOT)
+
+    def listdir(self):
+        # type: () -> List[unicode]
+        return [self._bundle]
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        return Info(_make_raw_dir_info(u''))
+
+
+class FSBundlePath(FSPath):
+    def __init__(self, fs, path, bundle):
+        # type: (OSFS, unicode, unicode) -> None
+        FSPath.__init__(self, fs, path)
+        assert path == join(_ROOT, bundle), '%s /= %s' % (path, bundle)
+        self._bundle = bundle
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        return Info(_make_raw_dir_info(self._bundle))
+
+    def openbin(self, mode, buffering, **options):
+        # type: (AnyStr, int, **Any) -> Any
+        raise FileExpected(self._original_path)
+
+    def listdir(self):
+        # type: () -> List[unicode]
+        dir_list = [info.name
+                    for info in self._wrap_fs.scandir(_ROOT)
+                    if info.is_dir]
+        dir_list.append(_VERSION_ONE)
+        return dir_list
+
+
+class FSCollectionPath(FSPath):
+    def __init__(self, fs, path, big_fs):
+        # type: (OSFS, unicode, ArchiveToVersionedFS) -> None
+        FSPath.__init__(self, fs, path)
+        self._big_fs = big_fs
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        return Info(_make_raw_dir_info(basename(self._original_path)))
+
+    def listdir(self):
+        # type: () -> List[unicode]
+        b, c = iteratepath(self._original_path)
+        dir_list = list(self._big_fs._get_collection_fits_products(
+                join(_ROOT, c)))
+        dir_list.append(_VERSION_ONE)
+        return dir_list
+
+
+class FSProductPath(FSPath):
+    def __init__(self, fs, path):
+        # type: (OSFS, unicode) -> None
+        FSPath.__init__(self, fs, path)
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        return Info(_make_raw_dir_info(basename(self._original_path)))
+
+    def listdir(self):
+        # type: () -> List[unicode]
+        return [_VERSION_ONE]
+
+
+class FSBundleVersionDirPath(FSPath):
+    def __init__(self, fs, path):
+        # type: (OSFS, unicode) -> None
+        FSPath.__init__(self, fs, path)
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        return Info(_make_raw_dir_info(basename(self._original_path)))
+
+    def listdir(self):
+        # type: () -> List[unicode]
+
+        # TODO: What if you've got a non-collection dir on the
+        # top-level?  Conceptually, it belongs to the bundle.  How do
+        # I distinguish?
+        infos = self._wrap_fs.filterdir(
+            _ROOT,
+            files=None,
+            dirs=None,
+            exclude_dirs=[ALL_PAT],
+            exclude_files=FILE_EXCLUSION_PATS)
+        return [info.name
+                for info in infos
+                if info.is_file]
+
+
+class FSCollectionVersionDirPath(FSPath):
+    def __init__(self, fs, path):
+        # type: (OSFS, unicode) -> None
+        FSPath.__init__(self, fs, path)
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        return Info(_make_raw_dir_info(basename(self._original_path)))
+
+    def listdir(self):
+        # type: () -> List[unicode]
+        b, c, v = iteratepath(self._original_path)
+        infos = self._wrap_fs.filterdir(
+            join(_ROOT, c),
+            files=None,
+            dirs=None,
+            exclude_dirs=[ALL_PAT],
+            exclude_files=FILE_EXCLUSION_PATS)
+        return [info.name
+                for info in infos
+                if info.is_file]
+
+
+class FSProductVersionDirPath(FSPath):
+    def __init__(self, fs, path, big_fs):
+        # type: (OSFS, unicode, ArchiveToVersionedFS) -> None
+        FSPath.__init__(self, fs, path)
+        self._big_fs = big_fs
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        b, c, p, v = iteratepath(self._original_path)
+        return Info(_make_raw_dir_info(v))
+
+    def listdir(self):
+        # type: () -> List[unicode]
+        b, c, p, v = iteratepath(self._original_path)
+        return self._big_fs._get_collection_product_files(join(_ROOT, c), p)
+
+
+class FSBundleVersionedFilePath(FSPath):
+    # /bundle/version/file
+    def __init__(self, fs, path):
+        # type: (OSFS, unicode) -> None
+        FSPath.__init__(self, fs, path)
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        b, v, f = iteratepath(self._original_path)
+        return self._wrap_fs.getinfo(join(_ROOT, f), namespaces)
+
+    def openbin(self, mode, buffering, **options):
+        # type: (AnyStr, int, **Any) -> Any
+        b, v, f = iteratepath(self._original_path)
+        return self._wrap_fs.openbin(join(_ROOT, f),
+                                     mode,
+                                     buffering,
+                                     **options)
+
+
+class FSCollectionVersionedFilePath(FSPath):
+    def __init__(self, fs, path, big_fs):
+        # type: (OSFS, unicode, ArchiveToVersionedFS) -> None
+        FSPath.__init__(self, fs, path)
+        self._big_fs = big_fs
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        b, c, v, f = iteratepath(self._original_path)
+        return self._wrap_fs.getinfo(join(_ROOT, c, f))
+
+    def openbin(self, mode, buffering, **options):
+        # type: (AnyStr, int, **Any) -> Any
+        b, c, v, f = iteratepath(self._original_path)
+        return self._wrap_fs.openbin(join(_ROOT, c, f),
+                                     mode, buffering, **options)
+
+
+class FSProductVersionedFilePath(FSPath):
+    def __init__(self, fs, path, big_fs):
+        # type: (OSFS, unicode, ArchiveToVersionedFS) -> None
+        FSPath.__init__(self, fs, path)
+        self._big_fs = big_fs
+
+    def getinfo(self, namespaces):
+        # type: (Tuple) -> Info
+        b, c, p, v, f = iteratepath(self._original_path)
+        fp = self._big_fs._get_product_filepath(join(_ROOT, c), f)
+        return self._wrap_fs.getinfo(fp)
+
+    def openbin(self, mode, buffering, **options):
+        # type: (AnyStr, int, **Any) -> Any
+        b, c, p, v, f = iteratepath(self._original_path)
+        fp = self._big_fs._get_product_filepath(join(_ROOT, c), f)
+        return self._wrap_fs.openbin(fp, mode, buffering, **options)
 
 
 class ArchiveToVersionedFS(ReadOnlyView):
@@ -66,6 +344,53 @@ class ArchiveToVersionedFS(ReadOnlyView):
             else:
                 raise ResourceNotFound(path)
         return res
+
+    def _make_fs_path(self, path):
+        # type: (unicode) -> FSPath
+        path = abspath(normpath(path))
+        parts = iteratepath(path)
+        l = len(parts)
+        if l == 0:
+            # synthetic root path
+            return FSRootPath(self._wrap_fs, _ROOT, self._bundle)
+        else:
+            if not (parts[0] == self._bundle):
+                # only files in the bundle exist in the filesystem
+                raise ResourceNotFound(path)
+            if l == 1:
+                # /bundle
+                return FSBundlePath(self._wrap_fs, path, self._bundle)
+            elif l == 2:
+                if _is_version_part(parts[1]):
+                    # /bundle/version
+                    return FSBundleVersionDirPath(self._wrap_fs, path)
+                else:
+                    # /bundle/collection
+                    return FSCollectionPath(self._wrap_fs, path, self)
+            elif l == 3:
+                if _is_version_part(parts[1]):
+                    # /bundle/version/file
+                    return FSBundleVersionedFilePath(self._wrap_fs, path)
+                elif _is_version_part(parts[2]):
+                    # /bundle/collection/version
+                    return FSCollectionVersionDirPath(self._wrap_fs, path)
+                else:
+                    # /bundle/collection/product
+                    return FSProductPath(self._wrap_fs, path)
+            elif l == 4:
+                if _is_version_part(parts[2]):
+                    # /bundle/collection/version/file
+                    return FSCollectionVersionedFilePath(self._wrap_fs,
+                                                         path, self)
+                else:
+                    # /bundle/collection/product/version
+                    assert _is_version_part(parts[3]), path
+                    return FSProductVersionDirPath(self._wrap_fs, path, self)
+            elif l == 5:
+                assert _is_version_part(parts[3])
+                return FSProductVersionedFilePath(self._wrap_fs, path, self)
+            else:
+                assert False, path
 
     def _get_visit_info(self, collection_path):
         return self._wrap_fs.filterdir(
@@ -116,25 +441,26 @@ class ArchiveToVersionedFS(ReadOnlyView):
     def getinfo(self, path, namespaces=None):
         self.check()
 
-        def make_raw_dir_info(name):
-            return {u'basic': {u'name': name, u'is_dir': True}}
+        fs_path = self._make_fs_path(path)
+        if fs_path:
+            return fs_path.getinfo(namespaces)
 
         parts = iteratepath(path)
         l = len(parts)
         if l == 0:
             # path = /; purely synthetic
-            raw_info = make_raw_dir_info(u'')
+            raw_info = _make_raw_dir_info(u'')
         else:
             if not (parts[0] == self._bundle):
                 raise ResourceNotFound(path)
             if l == 1:
                 # path = /b; orig path = /
-                raw_info = make_raw_dir_info(self._bundle)
+                raw_info = _make_raw_dir_info(self._bundle)
             elif l == 2:
                 _b, c = parts
                 if _is_version_part(c):
                     # path = b/$n
-                    raw_info = make_raw_dir_info(c)
+                    raw_info = _make_raw_dir_info(c)
                 else:
                     # path = b/c; orig path = c
                     with unwrap_errors(path):
@@ -150,10 +476,10 @@ class ArchiveToVersionedFS(ReadOnlyView):
                             raw_info = self._wrap_fs.getinfo(p).raw
                 elif _is_version_part(p):
                     # path = b/c/$n; orig path = /c
-                    raw_info = make_raw_dir_info(p)
+                    raw_info = _make_raw_dir_info(p)
                 else:
                     # path = b/c/p; synthetic
-                    raw_info = make_raw_dir_info(p)
+                    raw_info = _make_raw_dir_info(p)
             elif l == 4:
                 _b, c, p, f = parts
                 if _is_version_part(p):
@@ -165,7 +491,7 @@ class ArchiveToVersionedFS(ReadOnlyView):
                             raw_info = self._wrap_fs.getinfo(join(c, f)).raw
                 elif _is_version_part(f):
                     # path = b/c/p/$n; synthetic
-                    raw_info = make_raw_dir_info(f)
+                    raw_info = _make_raw_dir_info(f)
                 else:
                     assert False, 'getinfo(%s) has %d parts' % (path, l)
             elif l == 5:
@@ -182,9 +508,16 @@ class ArchiveToVersionedFS(ReadOnlyView):
         return Info(raw_info)
 
     def openbin(self, path, mode="r", buffering=-1, **options):
+        # type: (unicode, AnyStr, int, **Any) -> Any
         self.check()
         if check_writable(mode):
             raise ResourceReadOnly(path)
+
+        fs_path = self._make_fs_path(path)
+        if fs_path:
+            return fs_path.openbin(mode, buffering, **options)
+
+        print '#### openbin(%r, %r)' % (path, mode)
         if basename(path) == _VERSION_DICT:
             # is it the bundle or the collection?
             parts = iteratepath(path)
@@ -222,6 +555,11 @@ class ArchiveToVersionedFS(ReadOnlyView):
 
     def listdir(self, path):
         self.check()
+
+        fs_path = self._make_fs_path(path)
+        if fs_path:
+            return fs_path.listdir()
+
         parts = iteratepath(path)
         l = len(parts)
         if l == 0:
@@ -289,14 +627,24 @@ def test_fs():
 
 
 if __name__ == '__main__':
-    fs = ArchiveToVersionedFS(u'/Users/spaceman/Desktop/Archive/hst_14334')
+    fs1 = ArchiveToVersionedFS(u'/Users/spaceman/Desktop/Archive/hst_14334')
     # import pudb; pu.db
-    print "fs.listdir(u'/hst_14334') = %s" % list(fs.listdir(u'/hst_14334'))
-    print "fs.scandir(u'/hst_14334') = %s" % list(fs.scandir(u'/hst_14334'))
-    print "fs.filterdir(u'/hst_14334') = %s" % \
-        list(fs.filterdir(u'/hst_14334'))
-    fs.tree()
-    # print fs._get_collection_fits_products(u'data_wfc3_raw')
-    print fs._get_versions_dict(u'/hst_14334/v$1')
-    print fs._get_versions_dict(u'/hst_14334/data_wfc3_raw/v$1')
-    print fs
+    if False:
+        print "fs1.listdir(u'/hst_14334') = %s" % \
+            list(fs1.listdir(u'/hst_14334'))
+        print "fs1.scandir(u'/hst_14334') = %s" % \
+            list(fs1.scandir(u'/hst_14334'))
+        print "fs1.filterdir(u'/hst_14334') = %s" % \
+            list(fs1.filterdir(u'/hst_14334'))
+        print fs1._get_collection_fits_products(u'data_wfc3_raw')
+        print fs1._get_versions_dict(u'/hst_14334/v$1')
+        print fs1._get_versions_dict(u'/hst_14334/data_wfc3_raw/v$1')
+    print fs1
+    fs1.tree()
+
+    fs2 = TempFS(u'trash_me')
+    if True:
+        try:
+            copy_fs(fs1, fs2)
+        except:
+            traceback.print_exc(file=sys.stdout)
