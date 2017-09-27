c6fbae6efe98c21d6ac0dd74806a3cde01994691
Eric Nedervold <enedervold@seti.org>
2017-09-26 22:29:29 -0700

    Expanded MultiversionBundleFS.

--- pdart/fs/MultiversionBundleFS.py
+++ pdart/fs/MultiversionBundleFS.py
@@ -1,14 +1,15 @@
 from fs.path import join
 from typing import TYPE_CHECKING
 
-from VersionDirNames import vid_to_dir_name
+from pdart.fs.VersionDirNames import vid_to_dir_name
+from pdart.fs.VersionedFS import SUBDIR_VERSIONS_FILENAME
 
 if TYPE_CHECKING:
     from pdart.pds4.LIDVID import LIDVID
 
 
 class MultiversionBundleFS(object):
-    def lidvid_to_directory(self, lidvid):
+    def lidvid_to_files_directory(self, lidvid):
         """For a given LIDVID, give the directory that contains its files."""
         # type: (LIDVID) -> unicode
         lid = lidvid.lid()
@@ -16,8 +17,16 @@ class MultiversionBundleFS(object):
         if lidvid.is_bundle_lidvid():
             return join(u'/', lid.bundle_id, vid_to_dir_name(vid))
         elif lidvid.is_collection_lidvid():
-            return join(u'/', lid.bundle_id, lid.collection_id, vid_to_dir_name(vid))
+            return join(u'/', lid.bundle_id,
+                        lid.collection_id, vid_to_dir_name(vid))
         elif lidvid.is_product_lidvid():
-            return join(u'/', lid.bundle_id, lid.collection_id, lid.product_id, vid_to_dir_name(vid))
+            return join(u'/', lid.bundle_id, lid.collection_id,
+                        lid.product_id, vid_to_dir_name(vid))
         else:
-            assert False, "can't categorize %s as bundle, collection or product" % lid
+            assert False, \
+                "can't categorize %s as bundle, collection or product" % lid
+
+    def lidvid_to_subdir_versions_path(self, lidvid):
+        # type: (LIDVID) -> unicode
+        return join(self.lidvid_to_files_directory(lidvid),
+                    SUBDIR_VERSIONS_FILENAME)
--- pdart/fs/test_MultiversionBundleFS.py
+++ pdart/fs/test_MultiversionBundleFS.py
@@ -1,14 +1,34 @@
 import unittest
 
-from MultiversionBundleFS import *
+from pdart.fs.MultiversionBundleFS import *
 from pdart.pds4.LIDVID import LIDVID
 
 
 class TestMultiversionBundleFS(unittest.TestCase):
     def setUp(self):
+        # type: () -> None
         self.fs = MultiversionBundleFS()
 
-    def test_lidvid_to_directory(self):
-        self.assertEqual(u'/b/v$1', self.fs.lidvid_to_directory(LIDVID('urn:nasa:pds:b::1')))
-        self.assertEqual(u'/b/c/v$2', self.fs.lidvid_to_directory(LIDVID('urn:nasa:pds:b:c::2')))
-        self.assertEqual(u'/b/c/p/v$3', self.fs.lidvid_to_directory(LIDVID('urn:nasa:pds:b:c:p::3')))
+    def test_lidvid_to_files_directory(self):
+        # type: () -> None
+        self.assertEqual(u'/b/v$1',
+                         self.fs.lidvid_to_files_directory(
+                             LIDVID('urn:nasa:pds:b::1')))
+        self.assertEqual(u'/b/c/v$2',
+                         self.fs.lidvid_to_files_directory(
+                             LIDVID('urn:nasa:pds:b:c::2')))
+        self.assertEqual(u'/b/c/p/v$3',
+                         self.fs.lidvid_to_files_directory(
+                             LIDVID('urn:nasa:pds:b:c:p::3')))
+
+    def test_lidvid_to_subdir_versions_path(self):
+        # type: () -> None
+        self.assertEqual(u'/b/v$3/subdir$versions.txt',
+                         self.fs.lidvid_to_subdir_versions_path(
+                             LIDVID('urn:nasa:pds:b::3')))
+        self.assertEqual(u'/b/c/v$4/subdir$versions.txt',
+                         self.fs.lidvid_to_subdir_versions_path(
+                             LIDVID('urn:nasa:pds:b:c::4')))
+        self.assertEqual(u'/b/c/p/v$5/subdir$versions.txt',
+                         self.fs.lidvid_to_subdir_versions_path(
+                             LIDVID('urn:nasa:pds:b:c:p::5')))
