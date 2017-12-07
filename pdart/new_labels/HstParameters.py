bf2bc7db8b18cecd237d6290a4a20ef222f8e1dd
Eric Nedervold <enedervold@seti.org>
2017-12-07 11:23:56 -0800

    Added HstParameters to labels.

---
+++ pdart/new_labels/HstParameters.py
@@ -0,0 +1,493 @@
+"""
+Functionality to build an ``<hst:HST />`` XML element using a SQLite
+database.
+"""
+from typing import TYPE_CHECKING
+
+from pdart.new_labels.HstParametersXml import *
+from pdart.rules.Combinators import *
+
+if TYPE_CHECKING:
+    from typing import Any, Dict, List
+
+
+def get_repeat_exposure_count(product_id):
+    # type: (unicode) -> unicode
+    """
+    Return a placeholder integer for the ``<repeat_exposure_count
+    />`` XML element, noting the problem.
+    """
+    return placeholder_int(product_id, 'repeat_exposure_count')
+
+
+def get_subarray_flag(product_id):
+    # type: (unicode) -> unicode
+    """
+    Return placeholder text for the ``<subarray_flag />`` XML element,
+    noting the problem.
+    """
+    return placeholder(product_id, 'subarray_flag')
+
+
+##############################
+# get_aperture_type
+##############################
+
+
+def _get_aperture_type(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    if instrument == 'wfpc2':
+        return _get_aperture_type_placeholder(card_dicts, instrument,
+                                              product_id)
+    else:
+        return card_dicts[0]['APERTURE']
+
+
+def _get_aperture_type_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder(product_id, 'aperture_type')
+
+
+get_aperture_type = multiple_implementations(
+    'get_aperture_type',
+    _get_aperture_type,
+    _get_aperture_type_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return text for the ``<aperture_type />`` XML element.
+"""
+
+
+##############################
+# get_bandwidth
+##############################
+
+
+def _get_bandwidth(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    if instrument == 'wfpc2':
+        bandwid = float(card_dicts[0]['BANDWID'])
+        return str(bandwid * 1.e-4)
+
+
+def _get_bandwidth_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder_float(product_id, 'bandwidth')
+
+
+get_bandwidth = multiple_implementations(
+    'get_bandwidth',
+    _get_bandwidth,
+    _get_bandwidth_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return a float for the ``<bandwidth />`` XML element.
+"""
+
+
+##############################
+# get_center_filter_wavelength
+##############################
+
+def _get_center_filter_wavelength(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    if instrument == 'wfpc2':
+        centrwv = float(card_dicts[0]['CENTRWV'])
+        return str(centrwv * 1.e-4)
+    else:
+        raise Exception('Unhandled instrument %s' % instrument)
+
+
+def _get_center_filter_wavelength_placeholder(card_dicts,
+                                              instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder_float(product_id, 'center_filter_wavelength')
+
+
+get_center_filter_wavelength = multiple_implementations(
+    'get_center_filter_wavelength',
+    _get_center_filter_wavelength,
+    _get_center_filter_wavelength_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return a float for the ``<center_filter_wavelength />`` XML element.
+"""
+
+
+##############################
+# get_detector_id
+##############################
+
+def _get_detector_id(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    detector = card_dicts[0]['DETECTOR']
+    if instrument == 'wfpc2':
+        if detector == '1':
+            return 'PC1'
+        else:
+            return 'WF' + detector
+    else:
+        return detector
+
+
+def _get_detector_id_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder(product_id, 'detector_id')
+
+
+get_detector_id = multiple_implementations(
+    'get_detector_id',
+    _get_detector_id,
+    _get_detector_id_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return text for the ``<detector_id />`` XML element.
+"""
+
+
+##############################
+# get_exposure_duration
+##############################
+
+def _get_exposure_duration(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return str(card_dicts[0]['EXPTIME'])
+
+
+def _get_exposure_duration_placeholder(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return placeholder_float(product_id, 'exposure_duration')
+
+
+get_exposure_duration = multiple_implementations(
+    'get_exposure_duration',
+    _get_exposure_duration,
+    _get_exposure_duration_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode], unicode]
+"""
+Return a float for the ``<exposure_duration />`` XML element.
+"""
+
+
+##############################
+# get_exposure_type
+##############################
+
+def _get_exposure_type(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return card_dicts[0]['EXPFLAG']
+
+
+def _get_exposure_type_placeholder(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return placeholder(product_id, 'exposure_type')
+
+
+get_exposure_type = multiple_implementations(
+    'get_exposure_type',
+    _get_exposure_type,
+    _get_exposure_type_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode], unicode]
+"""
+Return text for the ``<exposure_type />`` XML element.
+"""
+
+
+##############################
+# get_filter_name
+##############################
+
+def _get_filter_name(card_dicts, instrument, product_name):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    if instrument == 'wfpc2':
+        filtnam1 = card_dicts[0]['FILTNAM1'].strip()
+        filtnam2 = card_dicts[0]['FILTNAM2'].strip()
+        if filtnam1 == '':
+            return filtnam2
+        elif filtnam2 == '':
+            return filtnam1
+        else:
+            return '%s+%s' % (filtnam1, filtnam2)
+    elif instrument == 'acs':
+        filter1 = card_dicts[0]['FILTER1']
+        filter2 = card_dicts[0]['FILTER2']
+        if filter1.startswith('CLEAR'):
+            if filter2.startswith('CLEAR'):
+                return 'CLEAR'
+            else:
+                return filter2
+        else:
+            if filter2.startswith('CLEAR'):
+                return filter1
+            else:
+                return '%s+%s' % (filter1, filter2)
+    elif instrument == 'wfc3':
+        return card_dicts[0]['FILTER']
+
+
+def _get_filter_name_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder(product_id, 'filter_name')
+
+
+get_filter_name = multiple_implementations(
+    'get_filter_name',
+    _get_filter_name,
+    _get_filter_name_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return text for the ``<filter_name />`` XML element.
+"""
+
+
+##############################
+# get_fine_guidance_system_lock_type
+##############################
+
+def _get_fine_guidance_system_lock_type(card_dicts, product_id):
+    return card_dicts[0]['FGSLOCK']
+
+
+def _get_fine_guidance_system_lock_type_placeholder(card_dicts, product_id):
+    return placeholder(product_id, 'fine_guidance_system_lock_type')
+
+
+get_fine_guidance_system_lock_type = multiple_implementations(
+    'get_fine_guidance_system_lock_type',
+    _get_fine_guidance_system_lock_type,
+    _get_fine_guidance_system_lock_type_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode], unicode]
+"""
+Return text for the ``<fine_guidance_system_lock_type />`` XML element.
+"""
+
+
+##############################
+# get_gain_mode_id
+##############################
+
+
+def _get_gain_mode_id(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    atodgain = card_dicts[0]['ATODGAIN']
+    if instrument == 'acs':
+        return str(atodgain)
+    elif instrument == 'wfpc2':
+        return 'A2D' + str(int(atodgain))
+
+
+def _get_gain_mode_id_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder(product_id, 'gain_mode_id')
+
+
+get_gain_mode_id = multiple_implementations(
+    'get_gain_mode_id',
+    _get_gain_mode_id,
+    _get_gain_mode_id_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return text for the ``<gain_mode_id />`` XML element.
+"""
+
+
+##############################
+# get_hst_pi_name
+##############################
+
+def _get_hst_pi_name(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    pr_inv_l = card_dicts[0]['PR_INV_L']
+    pr_inv_f = card_dicts[0]['PR_INV_F']
+    pr_inv_m = card_dicts[0]['PR_INV_M']
+    return '%s, %s %s' % (pr_inv_l, pr_inv_f, pr_inv_m)
+
+
+def _get_hst_pi_name_placeholder(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return placeholder(product_id, 'hst_pi_name')
+
+
+get_hst_pi_name = multiple_implementations(
+    'get_hst_pi_name',
+    _get_hst_pi_name,
+    _get_hst_pi_name_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode], unicode]
+"""
+Return text for the ``<hst_pi_name />`` XML element.
+"""
+
+
+##############################
+# get_hst_proposal_id
+##############################
+
+def _get_hst_proposal_id(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return str(card_dicts[0]['PROPOSID'])
+
+
+def _get_hst_proposal_id_placeholder(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return placeholder_int(product_id, 'hst_proposal_id')
+
+
+get_hst_proposal_id = multiple_implementations(
+    'get_hst_proposal_id',
+    _get_hst_proposal_id,
+    _get_hst_proposal_id_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode], unicode]
+"""
+Return text for the ``<hst_proposal_id />`` XML element.
+"""
+
+
+##############################
+# get_hst_target_name
+##############################
+
+def _get_hst_target_name(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return card_dicts[0]['TARGNAME']
+
+
+def _get_hst_target_name_placeholder(card_dicts, product_id):
+    # type: (List[Dict[str, Any]], unicode) -> unicode
+    return placeholder(product_id, 'hst_target_name')
+
+
+get_hst_target_name = multiple_implementations(
+    'get_hst_target_name',
+    _get_hst_target_name,
+    _get_hst_target_name_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode], unicode]
+"""
+Return text for the ``<hst_target_name />`` XML element.
+"""
+
+
+##############################
+# get_instrument_mode_id
+##############################
+
+def _get_instrument_mode_id(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    if instrument == 'wfpc2':
+        return card_dicts[0]['MODE']
+    else:
+        return card_dicts[0]['OBSMODE']
+
+
+def _get_instrument_mode_id_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder(product_id, 'instrument_mode_id')
+
+
+get_instrument_mode_id = multiple_implementations(
+    'get_instrument_mode_id',
+    _get_instrument_mode_id,
+    _get_instrument_mode_id_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return text for the ``<instrument_mode_id />`` XML element.
+"""
+
+
+##############################
+# get_observation_type
+##############################
+
+def _get_observation_type(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    if instrument != 'wfpc2':
+        return card_dicts[0]['OBSTYPE']
+    else:
+        raise Exception('Unhandled instrument %s' % instrument)
+
+
+def _get_observation_type_placeholder(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> unicode
+    return placeholder(product_id, 'observation_type')
+
+
+get_observation_type = multiple_implementations(
+    'get_observation_type',
+    _get_observation_type,
+    _get_observation_type_placeholder)
+# type: Callable[[List[Dict[str, Any]], unicode, unicode], unicode]
+"""
+Return text for the ``<observation_type />`` XML element.
+"""
+
+
+##############################
+
+def get_hst_parameters(card_dicts, instrument, product_id):
+    # type: (List[Dict[str, Any]], unicode, unicode) -> NodeBuilder
+    """Return an ``<hst:HST />`` XML element."""
+    d = {'stsci_group_id': known_placeholder(product_id, 'stsci_group_id'),
+         'hst_proposal_id': get_hst_proposal_id(card_dicts, product_id),
+         'hst_pi_name': get_hst_pi_name(card_dicts, product_id),
+         'hst_target_name': get_hst_target_name(card_dicts, product_id),
+         'aperture_type': get_aperture_type(card_dicts, instrument,
+                                            product_id),
+         'exposure_duration': get_exposure_duration(card_dicts, product_id),
+         'exposure_type': get_exposure_type(card_dicts, product_id),
+         'filter_name': get_filter_name(card_dicts, instrument, product_id),
+         'fine_guidance_system_lock_type':
+             get_fine_guidance_system_lock_type(card_dicts, product_id),
+         'gyroscope_mode': known_placeholder(product_id,
+                                             'gyroscope_mode'),
+         'instrument_mode_id': get_instrument_mode_id(card_dicts,
+                                                      instrument,
+                                                      product_id),
+         'moving_target_flag': 'true'}
+
+    if instrument == 'acs':
+        parameters_instrument = parameters_acs(
+            {'detector_id': get_detector_id(card_dicts, instrument,
+                                            product_id),
+             'gain_mode_id': get_gain_mode_id(card_dicts, instrument,
+                                              product_id),
+             'observation_type':
+                 get_observation_type(card_dicts, instrument, product_id),
+             'repeat_exposure_count': get_repeat_exposure_count(product_id),
+             'subarray_flag': get_subarray_flag(product_id)})
+    elif instrument == 'wfpc2':
+        header = None
+        # type: unicode
+        parameters_instrument = parameters_wfpc2(
+            {'bandwidth': get_bandwidth(card_dicts, instrument, product_id),
+             'center_filter_wavelength':
+                 get_center_filter_wavelength(card_dicts,
+                                              instrument, product_id),
+             'targeted_detector_id':
+                 get_targeted_detector_id(product_id, instrument,
+                                          header),
+             'gain_mode_id': get_gain_mode_id(card_dicts, instrument,
+                                              product_id),
+             'pc1_flag': get_pc1_flag(product_id, instrument,
+                                      header),
+             'wf2_flag': get_wf2_flag(product_id, instrument,
+                                      header),
+             'wf3_flag': get_wf3_flag(product_id, instrument,
+                                      header),
+             'wf4_flag': get_wf4_flag(product_id, instrument,
+                                      header)})
+    elif instrument == 'wfc3':
+        parameters_instrument = parameters_wfc3(
+            {'detector_id': get_detector_id(card_dicts,
+                                            instrument,
+                                            product_id),
+             'observation_type':
+                 get_observation_type(card_dicts, instrument, product_id),
+             'repeat_exposure_count':
+                 get_repeat_exposure_count(product_id),
+             'subarray_flag':
+                 get_subarray_flag(product_id)})
+    else:
+        assert False, 'Bad instrument value: %s' % instrument
+
+    return hst({
+        'parameters_general': parameters_general(d),
+        'parameters_instrument': parameters_instrument})
---
+++ pdart/new_labels/test_HstParameters.py
@@ -0,0 +1,86 @@
+import unittest
+
+from fs.path import basename, join
+
+from pdart.new_db.BundleDB import create_bundle_db_in_memory
+from pdart.new_db.FitsFileDB import card_dictionaries, populate_from_fits_file
+from pdart.new_db.SqlAlchTables import File, FitsFile
+from pdart.new_labels.HstParameters import *
+from pdart.xml.Pretty import pretty_print
+
+
+class Test_HstParameters(unittest.TestCase):
+    # TODO Write cases for other two instruments
+
+    # TODO get_gain_mode_id() is failing on this input file.  Investigate why.
+
+    def test_get_acs_parameters(self):
+        # type: () -> None
+        db = create_bundle_db_in_memory()
+        db.create_tables()
+        archive = '/Users/spaceman/Desktop/Archive'
+
+        fits_product_lidvid = \
+            'urn:nasa:pds:hst_13012:data_acs_raw:jbz504eoq_raw::2'
+        os_filepath = join(
+            archive,
+            'hst_13012/data_acs_raw/visit_04/jbz504eoq_raw.fits')
+
+        populate_from_fits_file(db,
+                                os_filepath,
+                                fits_product_lidvid)
+
+        file_basename = basename(os_filepath)
+
+        fits_file = db.session.query(FitsFile).filter(
+            File.product_lidvid == fits_product_lidvid).filter(
+            File.basename == file_basename).one()
+
+        hdu_count = fits_file.hdu_count
+
+        card_dicts = card_dictionaries(db.session,
+                                       fits_product_lidvid,
+                                       hdu_count)
+
+        nb = get_hst_parameters(card_dicts, u'acs', fits_product_lidvid)
+        doc = xml.dom.getDOMImplementation().createDocument(None, None, None)
+        str = nb(doc).toxml()
+        str = pretty_print(str)
+        print str
+        self.assertEqual(_get_expected(), str)
+
+
+def _get_expected():
+    # type: () -> str
+    # I'm jumping through hoops here just to get PEP8 compliance.
+    return '\n'.join([
+        '<?xml version="1.0"?>',
+        '<hst:HST>',
+        '  <hst:Parameters_General>',
+        '    <hst:stsci_group_id>### placeholder for stsci_group_id '
+        '###</hst:stsci_group_id>',
+        '    <hst:hst_proposal_id>13012</hst:hst_proposal_id>',
+        '    <hst:hst_pi_name>Lamy, Laurent </hst:hst_pi_name>',
+        '    <hst:hst_target_name>URANUS</hst:hst_target_name>',
+        '    <hst:aperture_type>SBC</hst:aperture_type>',
+        '    <hst:exposure_duration>270.0</hst:exposure_duration>',
+        '    <hst:exposure_type>NORMAL</hst:exposure_type>',
+        '    <hst:filter_name>F165LP+N/A</hst:filter_name>',
+        '    <hst:fine_guidance_system_lock_type>FINE</hst'
+        ':fine_guidance_system_lock_type>',
+        '    <hst:gyroscope_mode>### placeholder for gyroscope_mode '
+        '###</hst:gyroscope_mode>',
+        '    <hst:instrument_mode_id>ACCUM</hst:instrument_mode_id>',
+        '    <hst:moving_target_flag>true</hst:moving_target_flag>',
+        '  </hst:Parameters_General>',
+        '  <hst:Parameters_ACS>',
+        '    <hst:detector_id>SBC</hst:detector_id>',
+        '    <hst:gain_mode_id>### placeholder for gain_mode_id '
+        '###</hst:gain_mode_id>',
+        '    <hst:observation_type>IMAGING</hst:observation_type>',
+        '    <hst:repeat_exposure_count>0</hst:repeat_exposure_count>',
+        '    <hst:subarray_flag>### placeholder for subarray_flag ###'
+        '</hst:subarray_flag>',
+        '  </hst:Parameters_ACS>',
+        '</hst:HST>',
+        ''])
