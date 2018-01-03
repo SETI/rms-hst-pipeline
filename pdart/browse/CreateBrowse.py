dba53d2bc757e6e247e238c74cde49aa97b7d38a
Eric Nedervold <enedervold@seti.org>
2018-01-03 08:35:47 -0800

    Small tweaks and starting to include testfiles into the project.

--- pdart/browse/CreateBrowse.py
+++ pdart/browse/CreateBrowse.py
@@ -3,21 +3,22 @@ from typing import TYPE_CHECKING
 from pdart.new_db.BrowseFileDB import populate_database_from_browse_file
 
 if TYPE_CHECKING:
+    from fs.base import FS
     from pdart.new_db.BundleDB import BundleDB
 
 
 def create_browse_file_from_fits_file_and_populate_database(
-        db, fits_product_lid, collection_lidvid):
-    # type: (BundleDB, str, str) -> int
-    _create_browse_file_from_fits_file(fits_product_lid)
-    browse_product_lid = None  # type: str
+        fs, db, fits_product_lid, collection_lidvid):
+    # type: (FS, BundleDB, str, str) -> int
+    create_browse_file_from_fits_file(fs, fits_product_lid)
+    browse_product_lidvid = None  # type: str
     basename = None  # type: unicode
     byte_size = None  # type: int
     populate_database_from_browse_file(
-        db, browse_product_lid, collection_lidvid, basename, byte_size)
+        db, browse_product_lidvid, collection_lidvid, basename, byte_size)
     return 0  # TODO fix this
 
 
-def _create_browse_file_from_fits_file(fits_product_lid):
-    # type: (str) -> None
-    pass
+def create_browse_file_from_fits_file(fs, fits_product_lid):
+    # type: (FS, str) -> bool
+    return False
