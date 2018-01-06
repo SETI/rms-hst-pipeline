454ad304b94e0cb6d86ec67a351cfdaeaf26820b
Eric Nedervold <enedervold@seti.org>
2018-01-06 12:39:45 -0800

    Switched to "golden" tests for labels: comparisons against "golden files" in the filesystem, instead of hard-coded strings.

--- pdart/new_labels/Utils.py
+++ pdart/new_labels/Utils.py
@@ -1,3 +1,5 @@
+from fs.path import dirname, join
+
 from pdart.pds4.LIDVID import LIDVID
 
 
@@ -9,3 +11,14 @@ def lidvid_to_lid(lidvid):
 def lidvid_to_vid(lidvid):
     # type: (str) -> str
     return str(LIDVID(lidvid).vid())
+
+
+def golden_filepath(basename):
+    # type: (unicode) -> unicode
+    return join(dirname(__file__), basename)
+
+
+def golden_file_contents(basename):
+    # type: (unicode) -> unicode
+    with open(golden_filepath(basename)) as f:
+        return f.read()
--- pdart/new_labels/test_BrowseProductLabel.py
+++ pdart/new_labels/test_BrowseProductLabel.py
@@ -5,7 +5,7 @@ from fs.path import basename, join
 from pdart.new_db.BrowseFileDB import populate_database_from_browse_file
 from pdart.new_db.BundleDB import create_bundle_db_in_memory
 from pdart.new_labels.BrowseProductLabel import *
-from pdart.xml.Pretty import pretty_print
+from pdart.new_labels.Utils import golden_file_contents
 
 
 class Test_BrowseProductLabel(unittest.TestCase):
@@ -66,54 +66,6 @@ class Test_BrowseProductLabel(unittest.TestCase):
                                         browse_product_lidvid,
                                         browse_file_basename,
                                         True)
-        str = pretty_print(str)
-        print str
-        self.assertEqual(_expected, str)
 
-
-_expected = '\n'.join([
-    '<?xml version="1.0"?>',
-    '<?xml-model href="http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1700.sch"',
-    '            schematypens="http://purl.oclc.org/dsdl/schematron"?>',
-    '<Product_Browse xmlns="http://pds.nasa.gov/pds4/pds/v1" '
-    'xmlns:pds="http://pds.nasa.gov/pds4/pds/v1" '
-    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
-    'xsi:schemaLocation="http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_1700.xsd">',
-    '  <Identification_Area>',
-    '    <logical_identifier>urn:nasa:pds:hst_13012:browse_acs_raw'
-    ':jbz504eoq_raw</logical_identifier>',
-    '    <version_id>2.1</version_id>',
-    '    <title>This product contains a browse image of a raw image obtained '
-    'the HST Observing Program 13012.</title>',
-    '    <information_model_version>1.6.0.0</information_model_version>',
-    '    <product_class>Product_Browse</product_class>',
-    '    <Modification_History>',
-    '      <Modification_Detail>',
-    '        <modification_date>2016-04-20</modification_date>',
-    '        <version_id>0.1</version_id>',
-    '        <description>PDS4 version-in-development of the '
-    'product</description>',
-    '      </Modification_Detail>',
-    '    </Modification_History>',
-    '  </Identification_Area>',
-    '  <Reference_List>',
-    '    <Internal_Reference>',
-    '      <lidvid_reference>urn:nasa:pds:hst_13012:data_acs_raw'
-    ':jbz504eoq_raw::2.1'
-    '</lidvid_reference>',
-    '      <reference_type>browse_to_data</reference_type>',
-    '    </Internal_Reference>',
-    '  </Reference_List>',
-    '  <File_Area_Browse>',
-    '    <File>',
-    '      <file_name>jbz504eoq_raw.jpg</file_name>',
-    '    </File>',
-    '    <Encoded_Image>',
-    '      <offset unit="byte">0</offset>',
-    '      <object_length unit="byte">5492356</object_length>',
-    '      <encoding_standard_id>JPEG</encoding_standard_id>',
-    '    </Encoded_Image>',
-    '  </File_Area_Browse>',
-    '</Product_Browse>',
-    ''])  # type: str
-# I'm jumping through hoops here just to get PEP8 compliance.
+        expected = golden_file_contents('test_BrowseProductLabel.golden.xml')
+        self.assertEqual(expected, str)
