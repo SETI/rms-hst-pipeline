e18dabb69f73991fb3635a2c008fdc4353833535
Eric Nedervold <enedervold@seti.org>
2018-01-11 15:19:46 -0800

    Fixed the LID vs. LIDVID issue in inventories.

--- pdart/new_labels/CollectionInventory.py
+++ pdart/new_labels/CollectionInventory.py
@@ -4,8 +4,6 @@ Functionality to build a collection inventory using a SQLite database.
 
 from typing import TYPE_CHECKING
 
-from pdart.new_labels.Utils import lidvid_to_lid
-
 if TYPE_CHECKING:
     from pdart.new_db.BundleDB import BundleDB
 
@@ -14,6 +12,6 @@ def make_collection_inventory(bundle_db, collection_lidvid):
     # type: (BundleDB, str) -> unicode
     products = bundle_db.get_collection_products(
         collection_lidvid)
-    inventory_lines = [u'P,%s\r\n' % lidvid_to_lid(product.lidvid)
+    inventory_lines = [u'P,%s\r\n' % product.lidvid
                        for product in products]
     return ''.join(inventory_lines)
