fd00c457ff9367a775ee5e725e07ad3691b16a64
Eric Nedervold <enedervold@seti.org>
2018-05-25 17:47:33 -0700

    Expanded Astroquery testing.

--- pdart/astroquery/test_Utils.py
+++ pdart/astroquery/test_Utils.py
@@ -17,11 +17,15 @@ class TestUtils(unittest.TestCase):
         self.assertEqual('2017-08-21T17:17:00', ymdhms)
 
     def test_filter_table(self):
+        # type: () -> None
+
+        # I'm using MastSlice to get a table, but it would probably be
+        # better to hand-construct one.
         from pdart.astroquery.Astroquery import MastSlice
         start_date = (1900, 1, 1)
         end_date = (2018, 3, 26)
         slice = MastSlice(start_date, end_date)
-        proposal_id = slice.observations_table[0]['proposal_id']
+        proposal_id = int(slice.observations_table[0]['proposal_id'])
 
         def proposal_id_matches(row):
             # test: (Row) -> bool
@@ -30,4 +34,10 @@ class TestUtils(unittest.TestCase):
         proposal_table = filter_table(proposal_id_matches,
                                       slice.observations_table)
 
+        # We know that it drops at least the first row because that's
+        # where we got the proposal_id from.
         self.assertTrue(len(proposal_table) < len(slice.observations_table))
+
+        # Make sure we got what we filtered for.
+        for row in proposal_table:
+            self.assertEqual(proposal_id, int(row['proposal_id']))
