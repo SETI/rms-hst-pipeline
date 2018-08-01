429d0c47cd85ad2c5c63b19589a3793d9ed90df5
Eric Nedervold <enedervold@seti.org>
2018-08-01 10:28:46 -0700

    Clean-up.

---
+++ pdart/archive/__init__.py
@@ -0,0 +1,8 @@
+"""
+The pdart.archive package contains code to manage the PDART archives.
+An archive is a directory containing multiple bundle archives.  A
+bundle archive is a multi-version directory that contains the files
+for a bundle, including its database file.
+
+**TODO** Write more documentation.
+"""
