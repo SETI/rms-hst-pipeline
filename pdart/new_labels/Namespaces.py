75f53c9655b11a71070aa3141a6e592c35823b03
Eric Nedervold <enedervold@seti.org>
2018-09-12 19:14:45 -0700

    Refactoring the contents of the namespace attributes.

--- pdart/new_labels/Namespaces.py
+++ pdart/new_labels/Namespaces.py
@@ -1,33 +1,64 @@
 from pdart.xml.Pds4Version import HST_SHORT_VERSION, PDS4_SHORT_VERSION
 
-BUNDLE_NAMESPACES = """xmlns="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:pds="http://pds.nasa.gov/pds4/pds/v1"
-"""  # type:str
+_PDS4_SCHEMA_LOCATION = "http://pds.nasa.gov/pds4/pds/v1"
 
-BROWSE_PRODUCT_NAMESPACES = """xmlns="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:pds="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_%s.xsd"
-""" % (PDS4_SHORT_VERSION,)  # type:str
+_VERSIONED_PDS4_SCHEMA_LOCATION = \
+    "https://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_%s.xsd" % \
+    (PDS4_SHORT_VERSION,)
 
-COLLECTION_NAMESPACES = """xmlns="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:pds="http://pds.nasa.gov/pds4/pds/v1"
-"""  # type: str
+_HST_SCHEMA_LOCATION = "http://pds.nasa.gov/pds4/mission/hst/v1"
 
-DOCUMENT_PRODUCT_NAMESPACES = """xmlns="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:hst="http://pds.nasa.gov/pds4/hst/v0"
-xmlns:pds="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://pds.nasa.gov/pds4/pds/v1 \
-http://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_%s.xsd"
-""" % (PDS4_SHORT_VERSION,)  # type: str
+_VERSIONED_HST_SCHEMA_LOCATION = \
+    "https://pds.nasa.gov/pds4/mission/hst/v1/PDS4_HST_%s.xsd" % \
+    (HST_SHORT_VERSION,)
 
-FITS_PRODUCT_NAMESPACES = """xmlns="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:hst="http://pds.nasa.gov/pds4/mission/hst/v1"
-xmlns:pds="http://pds.nasa.gov/pds4/pds/v1"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://pds.nasa.gov/pds4/pds/v1 \
-https://pds.nasa.gov/pds4/pds/v1/PDS4_PDS_%s.xsd \
-http://pds.nasa.gov/pds4/mission/hst/v1 \
-https://pds.nasa.gov/pds4/mission/hst/v1/PDS4_HST_%s.xsd"
-""" % (PDS4_SHORT_VERSION, HST_SHORT_VERSION)  # type: str
+
+def make_schema_locations(*args):
+    return 'xsi:schemaLocation="%s"' % ' '. join(args)
+
+
+def make_namespaces(*parts):
+    return ' '.join(parts)
+
+
+_PDS4_NAMESPACE = 'xmlns="http://pds.nasa.gov/pds4/pds/v1"'  # type: str
+
+_HST_NAMESPACE = \
+    'xmlns:hst="http://pds.nasa.gov/pds4/mission/hst/v1"'  # type: str
+
+_PDS_NAMESPACE = 'xmlns:pds="http://pds.nasa.gov/pds4/pds/v1"'  # type: str
+
+_XSI_NAMESPACE = \
+    'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'  # type: str
+
+BUNDLE_NAMESPACES = make_namespaces(_PDS4_NAMESPACE,
+                                    _PDS_NAMESPACE)
+
+BROWSE_PRODUCT_NAMESPACES = \
+    make_namespaces(_PDS4_NAMESPACE,
+                    _PDS_NAMESPACE,
+                    _XSI_NAMESPACE,
+                    make_schema_locations(_VERSIONED_PDS4_SCHEMA_LOCATION))
+
+COLLECTION_NAMESPACES = make_namespaces(_PDS4_NAMESPACE,
+                                        _PDS_NAMESPACE)
+
+DOCUMENT_PRODUCT_NAMESPACES = make_namespaces(
+    _PDS4_NAMESPACE,
+    _PDS_NAMESPACE,
+    _HST_NAMESPACE,
+    _XSI_NAMESPACE,
+    make_schema_locations(
+        _PDS4_SCHEMA_LOCATION,
+        _VERSIONED_PDS4_SCHEMA_LOCATION))
+
+FITS_PRODUCT_NAMESPACES = make_namespaces(
+    _PDS4_NAMESPACE,
+    _PDS_NAMESPACE,
+    _HST_NAMESPACE,
+    _XSI_NAMESPACE,
+    make_schema_locations(
+        _PDS4_SCHEMA_LOCATION,
+        _VERSIONED_PDS4_SCHEMA_LOCATION,
+        _HST_SCHEMA_LOCATION,
+        _VERSIONED_HST_SCHEMA_LOCATION))
