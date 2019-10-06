d63d8ed7269d5e0dc8334d1eb22b21c33ac27513
Eric Nedervold <enedervold@seti.org>
2019-10-06 15:13:30 -0700

    Tweaked the add_pds_tools hack for BuildBot.

--- pdart/add_pds_tools.py
+++ pdart/add_pds_tools.py
@@ -1,7 +1,7 @@
 """
 Importing this module appends the path for the ``pds-tools`` package
-directory to the system path.  I assume that ``pds-tools`` lives in
-the developer's home directory.
+directory to the system path.  I assume that ``pds-tools`` lives in a
+sibling directory.
 
 This is a temporary hack until I figure out the correct way to
 generally combine my packages with the ``pds-tools`` modules.
@@ -10,6 +10,6 @@ import os.path
 import sys
 
 
-_PDS_TOOLS_DIR = os.path.join(os.path.expanduser('~'), 'pds-tools')
+_PDS_TOOLS_DIR = os.path.join('../pds-tools')
 
 sys.path.append(_PDS_TOOLS_DIR)
