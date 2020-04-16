680cff435ba312f15974cd7ea679550b94f72964
Eric Nedervold <enedervold@seti.org>
2020-04-15 21:36:49 -0700

    Refactoring the pipeline.  Backed out some suggested changes in validate-pdart; some unusual interface choices of the validation tool made the changes suggested by shellcheck actually wrong.

--- pdart-py3/Pipeline.py
+++ pdart-py3/Pipeline.py
@@ -16,44 +16,39 @@ from pdart.pipeline.RecordChanges import RecordChanges
 from pdart.pipeline.ResetPipeline import ResetPipeline
 from pdart.pipeline.UpdateArchive import UpdateArchive
 from pdart.pipeline.ValidateBundle import ValidateBundle
+from pdart.pipeline.Stage import FAILURE_MARKER
 
 
-def dispatch(dirs: Directories, proposal_id: int, command: str) -> None:
-    bundle_segment = f"hst_{proposal_id:05}"
+_STAGE = Callable[[], None]
 
-    # Here's a list of all the commands available.
-    command_dict: Dict[str, Callable[[], None]] = {
-        # Clear everything except for the cached downloads
-        "reset_pipeline": ResetPipeline(bundle_segment, dirs, proposal_id),
-        # Download document files.
-        "download_docs": DownloadDocs(bundle_segment, dirs, proposal_id),
-        # Download FITS files.
-        "check_downloads": CheckDownloads(bundle_segment, dirs, proposal_id),
-        # Copy primary files (documents and FITS data files) into a
-        # staging area.
-        "copy_primary_files": CopyPrimaryFiles(bundle_segment, dirs, proposal_id),
-        # Note which files have changes and save this information into
-        # a file.
-        "record_changes": RecordChanges(bundle_segment, dirs, proposal_id),
-        # Insert the actual changes into (a layer over) the archive.
-        "insert_changes": InsertChanges(bundle_segment, dirs, proposal_id),
-        # Fill the database with information from new primary files.
-        "populate_database": PopulateDatabase(bundle_segment, dirs, proposal_id),
-        # Build a browse collection for each new data collection.
-        "build_browse": BuildBrowse(bundle_segment, dirs, proposal_id),
-        # Build labels for the new components.
-        "build_labels": BuildLabels(bundle_segment, dirs, proposal_id),
-        # Update the archive with the new version
-        "update_archive": UpdateArchive(bundle_segment, dirs, proposal_id),
-        # Build labels for the new components.
-        "make_deliverable": MakeDeliverable(bundle_segment, dirs, proposal_id),
-        # Run the validation tool on the deliverable bundle
-        "validate_bundle": ValidateBundle(bundle_segment, dirs, proposal_id),
-    }
-    command_dict[command]()
-
-
-_FAILURE_MARKER: str = "LAST$FAILURE.txt"
+_COMMAND_DICT: Dict[str, Callable[[Directories, int], _STAGE]] = {
+    # Clear everything except for the cached downloads
+    "reset_pipeline": ResetPipeline,
+    # Download document files.
+    "download_docs": DownloadDocs,
+    # Download FITS files.
+    "check_downloads": CheckDownloads,
+    # Copy primary files (documents and FITS data files) into a
+    # staging area.
+    "copy_primary_files": CopyPrimaryFiles,
+    # Note which files have changes and save this information into
+    # a file.
+    "record_changes": RecordChanges,
+    # Insert the actual changes into (a layer over) the archive.
+    "insert_changes": InsertChanges,
+    # Fill the database with information from new primary files.
+    "populate_database": PopulateDatabase,
+    # Build a browse collection for each new data collection.
+    "build_browse": BuildBrowse,
+    # Build labels for the new components.
+    "build_labels": BuildLabels,
+    # Update the archive with the new version
+    "update_archive": UpdateArchive,
+    # Build labels for the new components.
+    "make_deliverable": MakeDeliverable,
+    # Run the validation tool on the deliverable bundle
+    "validate_bundle": ValidateBundle,
+}
 
 
 def run() -> None:
@@ -62,22 +57,7 @@ def run() -> None:
     command = sys.argv[2]
 
     dirs = make_directories()
-    failure_marker_filepath = os.path.join(
-        dirs.working_dir(proposal_id), _FAILURE_MARKER
-    )
-
-    if os.path.isfile(failure_marker_filepath):
-        print(f"**** Skipping hst_{proposal_id:05} {command} due to previous failures.")
-        return
-
-    try:
-        dispatch(dirs, proposal_id, command)
-    except Exception as e:
-        with open(failure_marker_filepath, "w") as f:
-            header = f"EXCEPTION raised by hst_{proposal_id:05}, stage {command}:"
-            print("****", header, str(e))
-            print(header, file=f)
-            print(traceback.format_exc(), file=f)
+    _COMMAND_DICT[command](dirs, proposal_id)()
 
 
 if __name__ == "__main__":
--- pdart-py3/pdart/pipeline/Stage.py
+++ pdart-py3/pdart/pipeline/Stage.py
@@ -1,15 +1,43 @@
 import abc
+import os
+import traceback
 
 from pdart.pipeline.Directories import Directories
 
+FAILURE_MARKER: str = "LAST$FAILURE.txt"
+
 
 class Stage(metaclass=abc.ABCMeta):
-    def __init__(
-        self, bundle_segment: str, dirs: Directories, proposal_id: int
-    ) -> None:
-        self._bundle_segment = bundle_segment
+    def __init__(self, dirs: Directories, proposal_id: int) -> None:
+        self._bundle_segment = f"hst_{proposal_id:05}"
         self._dirs = dirs
         self._proposal_id = proposal_id
+
+    def class_name(self) -> str:
+        return type(self).__name__
+
+    ##############################
+
+    def __call__(self) -> None:
+        failure_marker_filepath = os.path.join(self.working_dir(), FAILURE_MARKER)
+        if not os.path.isfile(failure_marker_filepath):
+            try:
+                self._run()
+            except Exception as e:
+                with open(failure_marker_filepath, "w") as f:
+                    header = (
+                        f"EXCEPTION raised by {self._bundle_segment}, "
+                        f"stage {self.class_name()}:"
+                    )
+                    print("****", header, str(e))
+                    print(header, file=f)
+                    print(traceback.format_exc(), file=f)
+
+    @abc.abstractmethod
+    def _run(self) -> None:
+        pass
+
+    ##############################
 
     def working_dir(self) -> str:
         return self._dirs.working_dir(self._proposal_id)
@@ -37,12 +65,3 @@ class Stage(metaclass=abc.ABCMeta):
 
     def deliverable_dir(self) -> str:
         return self._dirs.deliverable_dir(self._proposal_id)
-
-    ##############################
-
-    def __call__(self) -> None:
-        self._run()
-
-    @abc.abstractmethod
-    def _run(self) -> None:
-        pass
