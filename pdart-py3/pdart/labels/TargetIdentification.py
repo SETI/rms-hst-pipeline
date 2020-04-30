0acf27bf15ced6cf8c8ccbd2cd258137147b6158
Eric Nedervold <enedervold@seti.org>
2020-04-30 16:34:01 -0700

    Temporarily put in a placeholder (Magrathea) for targets we cannot identify to allow work on other parts of the pipeline to continue.

--- pdart-py3/pdart/labels/TargetIdentification.py
+++ pdart-py3/pdart/labels/TargetIdentification.py
@@ -13,6 +13,8 @@ from pdart.labels.TargetIdentificationXml import (
 )
 from pdart.xml.Templates import NodeBuilder
 
+_USING_PLACEHOLDER: bool = True
+
 
 def get_target_info(card_dictionaries: List[Dict[str, Any]]) -> Dict[str, str]:
     targname = card_dictionaries[0]["TARGNAME"]
@@ -25,6 +27,17 @@ def get_target_info(card_dictionaries: List[Dict[str, Any]]) -> Dict[str, str]:
                 "description": f"The {type.lower()} {name}",
                 "lid": target_lid(name, type),
             }
+    if _USING_PLACEHOLDER:
+        # TODO-PLACEHOLER
+        name = "Magrathea"
+        type = "Planet"
+        return {
+            "name": name,
+            "type": type,
+            "description": f"The {type.lower()} {name}",
+            "lid": target_lid(name, type),
+        }
+
     raise ValueError(f"TARGNAME {targname} doesn't match approximations")
 
 
