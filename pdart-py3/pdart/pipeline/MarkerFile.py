ad82d650b18a0f9c6d509b0a07ecc25806ecf2af
Eric Nedervold <enedervold@seti.org>
2020-05-06 14:08:00 -0700

    Can now write marker files to note the process of the pipeline.

---
+++ pdart-py3/pdart/pipeline/MarkerFile.py
@@ -0,0 +1,73 @@
+import abc
+from collections import namedtuple
+import os
+import os.path
+import re
+from typing import Optional
+
+
+class MarkerInfo(object):
+    def __init__(self, phase: str, state: str, text: Optional[str]):
+        self.phase = phase.upper()
+        self.state = state.upper()
+        self.text = text
+
+    def __str__(self) -> str:
+        return f"MarkerInfo({self.phase!r}, {self.state!r}, {self.text!r})"
+
+
+############################################################
+
+
+class MarkerFile(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def clear_marker(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def get_marker(self) -> Optional[MarkerInfo]:
+        pass
+
+    @abc.abstractmethod
+    def set_marker(self, marker_info: MarkerInfo) -> None:
+        pass
+
+
+_RE = r"^#([A-Z_]+)#([A-Z_]+)#\.txt$"
+
+
+def _info_to_path(info: MarkerInfo) -> str:
+    return f"#{info.phase}#{info.state}#.txt"
+
+
+class BasicMarkerFile(MarkerFile):
+    def __init__(self, directory: str) -> None:
+        self._directory = directory
+
+    def _path_to_info(self, path: str) -> Optional[MarkerInfo]:
+        m = re.match(_RE, os.path.basename(path))
+        if m:
+            with open(os.path.join(self._directory, path), "r") as f:
+                text = f.read()
+            return MarkerInfo(m.group(1), m.group(2), text)
+        else:
+            return None
+
+    def get_marker(self) -> Optional[MarkerInfo]:
+        for path in os.listdir(self._directory):
+            res = self._path_to_info(path)
+            if res:
+                return res
+        return None
+
+    def clear_marker(self) -> None:
+        old_info = self.get_marker()
+        if old_info:
+            old_path: str = os.path.join(self._directory, _info_to_path(old_info))
+            os.remove(old_path)
+
+    def set_marker(self, new_info: MarkerInfo) -> None:
+        self.clear_marker()
+        with open(os.path.join(self._directory, _info_to_path(new_info)), "w") as f:
+            if new_info.text:
+                f.write(new_info.text)
