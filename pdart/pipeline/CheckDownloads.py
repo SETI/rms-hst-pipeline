f623d62aceab2cb825d2409bb49a4af31b74a411
Yu-Jen Chang <juzen2003@gmail.com>
2022-01-26 12:56:34 -0800

    Replace assert with raise exception in pipeline/CheckDownloads.py

--- pdart/pipeline/CheckDownloads.py
+++ pdart/pipeline/CheckDownloads.py
@@ -36,13 +36,15 @@ class CheckDownloads(MarkedStage):
         PDS_LOGGER.open("Download datafiles")
         # first pass, <working_dir> shouldn't exist; second pass
         # <working_dir>/mastDownload should not exist.
-        assert not os.path.isdir(mast_downloads_dir)
+        if os.path.isdir(mast_downloads_dir):
+            raise ValueError("<working_dir>/mastDownload should not exist.")
 
         # TODO These dates are wrong; they potentially collect too
         # much.  Do I need to reduce the range of dates here?
         slice = MastSlice((1900, 1, 1), (2025, 1, 1), proposal_id)
         proposal_ids = slice.get_proposal_ids()
-        assert proposal_id in proposal_ids, f"{proposal_id} in {proposal_ids}"
+        if proposal_id not in proposal_ids:
+            raise KeyError(f"{proposal_id} not in {proposal_ids}")
         # get files from full list of ACCEPTED_SUFFIXES
         product_set = slice.to_product_set(proposal_id)
         if not os.path.isdir(working_dir):
@@ -53,18 +55,20 @@ class CheckDownloads(MarkedStage):
 
         # TODO This might fail if there are no files.  Which might not be
         # a bad thing.
-        print(f"::::::::::mast_downloads_dir: {mast_downloads_dir}")
+        PDS_LOGGER.log("info", f"::::::::::mast_downloads_dir: {mast_downloads_dir}")
         PDS_LOGGER.log("info", f"Download datafiles to {mast_downloads_dir}")
-        assert os.path.isdir(mast_downloads_dir)
+        if not os.path.isdir(mast_downloads_dir):
+            raise ValueError(f"{mast_downloads_dir} doesn't exist.")
         PDS_LOGGER.close()
 
     def _run(self) -> None:
         working_dir: str = self.working_dir()
         mast_downloads_dir: str = self.mast_downloads_dir()
 
-        assert not os.path.isdir(
-            self.deliverable_dir()
-        ), f"{self.deliverable_dir()} cannot exist for CheckDownloads"
+        if os.path.isdir(self.deliverable_dir()):
+            raise ValueError(
+                f"{self.deliverable_dir()} cannot exist " + "for CheckDownloads"
+            )
 
         if not os.path.isdir(mast_downloads_dir):
             self._do_downloads(
