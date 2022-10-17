a4d69f054b1873b5e16c8297dbcf5afbd10fa4a4
Mark Showalter <mshowalter@seti.org>
2022-10-16 19:56:55 -0700

    Remove old root-level copy of target_identifications

--- target_identifications/__init__.py
+++
@@ -1,8 +0,0 @@
-# type: ignore
-################################################################################
-# __init__.py for Target_Identification module
-################################################################################
-
-from .standard_bodies import standard_body_identifications
-from .minor_planets import minor_planet_identifications
-from .comets import comet_identifications
