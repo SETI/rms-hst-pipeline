3c2c0e4373d2ed0dbc773762ad80b6fbcccb6a53
Dave Chang <juzen2003@gmail.com>
2025-07-01 15:19:05 -0700

    Add more tests to cover all the code in query_hst_moving_targets.py

--- HST/tests/test_query_hst_moving_targets.py
+++ HST/tests/test_query_hst_moving_targets.py
@@ -5,18 +5,28 @@
 ##########################################################################################
 
 import pytest
+from hst_helper import (START_DATE,
+                        END_DATE)
 from query_hst_moving_targets import query_hst_moving_targets
 
 class TestQueryHSTMovingTargets:
     @pytest.mark.parametrize(
-        'p_ids,inst,expected',
+        'p_ids,inst,start_date,end_date,expected',
         [
-            ([], ['NICMOS/NIC1'], 24),
-            (['7885'], [], 1),
-            (['15505'], ['WFC3/UVIS'], 1),
+            ([], ['NICMOS/NIC1'], START_DATE, END_DATE, 24),
+            ([], ['NICMOS/NIC1'], (1900, 1, 1), (1900, 1, 2), 0),
+            (['7885'], [], START_DATE, END_DATE, 1),
+            (['06774'], [], (1900, 1, 1), (1900, 1, 2), 0),
+            (['15505'], ['WFC3/UVIS'], START_DATE, END_DATE, 1),
+            (['15505'], ['WFC3/UVIS'], (1900, 1, 1), (1900, 1, 2), 0),
+            ([], [], (1900, 1, 1), (1900, 1, 2), 0),
+            ([], [], (1998, 1, 1), (1999, 1, 1), 42),
         ],
     )
-    def test_query_hst_moving_targets(self, p_ids, inst, expected):
-        id_li = query_hst_moving_targets(proposal_ids=p_ids, instruments=inst)
+    def test_query_hst_moving_targets(self, p_ids, inst, start_date, end_date, expected):
+        id_li = query_hst_moving_targets(proposal_ids=p_ids,
+                                         instruments=inst,
+                                         start_date=start_date,
+                                         end_date=end_date)
         print(len(id_li))
         assert len(id_li) == expected
